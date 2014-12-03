29a05d0860a409edf4b3ac2a1c1e78707ac94592
Mateus Brandao <mateusfbi@gmail.com>
2014-12-03 19:44:08 -0300

    File Translation "imglib_lang.php"

---
+++ language/portuguese-brazilian/imglib_lang.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * CodeIgniter
+ *
+ * An open source application development framework for PHP 5.2.4 or newer
+ *
+ * This content is released under the MIT License (MIT)
+ *
+ * Copyright (c) 2014, British Columbia Institute of Technology
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @package	CodeIgniter
+ * @author	EllisLab Dev Team
+ * @copyright	Copyright (c) 2008 - 2014, EllisLab, Inc. (http://ellislab.com/)
+ * @copyright	Copyright (c) 2014, British Columbia Institute of Technology (http://bcit.ca/)
+ * @license	http://opensource.org/licenses/MIT	MIT License
+ * @link	http://codeigniter.com
+ * @since	Version 1.0.0
+ * @filesource
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['imglib_source_image_required'] = 'Você deve especificar a origem da imagem em suas preferências.';
+$lang['imglib_gd_required'] = 'A Bilblioteca "GD image library" é necessária para está funcionalidade.';
+$lang['imglib_gd_required_for_props'] = 'Seu servidor deve suportar a Bilblioteca GD image library em razão de determinar as propriedades da imagem.';
+$lang['imglib_unsupported_imagecreate'] = 'Seu servidor não suporta a função GD necessária para processar este tipo de imagem.';
+$lang['imglib_gif_not_supported'] = 'Imagens GIF geralmente não são suportadas devido à restrições de licenças. Você deve usar JPG ou PNG.';
+$lang['imglib_jpg_not_supported'] = 'Imagens JPG não são suportadas.';
+$lang['imglib_png_not_supported'] = 'Imagens PNG não são suportadas.';
+$lang['imglib_jpg_or_png_required'] = 'O protocolo de redimencionamento de imagem especificado em suas preferências funciona somente com imagens JPEG ou PNG.';
+$lang['imglib_copy_error'] = 'Um erro foi encontrado enquanto tentava substituir um arquivo. Por favor tenha certeza que seu diretório é gravável.';
+$lang['imglib_rotate_unsupported'] = 'Rotação de imagem parece não ser suportada por seu servidor.';
+$lang['imglib_libpath_invalid'] = 'O caminho para a biblioteca de imagens não está correto.  Por favor configure o caminho correto em suas preferências de imagens.';
+$lang['imglib_image_process_failed'] = 'Falha ao processar a imagem. Por favor verifique que seu servidor suporta o protocolo escolhido e que o caminho para sua biblioteca de imagens esteja correto.';
+$lang['imglib_rotation_angle_required'] = 'O ângulo de rotação é obrigatório para rodar a imagem.';
+$lang['imglib_writing_failed_gif'] = 'Imagem GIF.';
+$lang['imglib_invalid_path'] = 'O caminho para a imagem não está correto';
+$lang['imglib_copy_failed'] = 'Failed, the image copy routine.';
+$lang['imglib_missing_font'] = 'Não é possível encontrar a fonte para usar.';
+$lang['imglib_save_failed'] = 'Não é possível salvar a imagem.  Por favor tenha certeza que a imagem e o diretório são graváveis.';
+
+/* End of file imglib_lang.php */
+/* Location: ./application/language/portuguese-brazilian/imglib_lang.php */
