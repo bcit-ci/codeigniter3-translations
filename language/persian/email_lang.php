1a9533c40e3bcbb62947448d082df651f834f669
MSDehghan <msdn2013@gmail.com>
2014-12-04 16:25:40 +0330

    some persian phrases translated

---
+++ language/persian/email_lang.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * CodeIgniter
+ *
+ * An open source application development framework for PHP 5.2.4 or newer
+ *
+ * This content is released under the MIT License (MIT)
+ *
+ * Copyright (c) 2014, British Columbia Institute of Technology
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @package	CodeIgniter
+ * @author	EllisLab Dev Team
+ * @copyright	Copyright (c) 2008 - 2014, EllisLab, Inc. (http://ellislab.com/)
+ * @copyright	Copyright (c) 2014, British Columbia Institute of Technology (http://bcit.ca/)
+ * @license	http://opensource.org/licenses/MIT	MIT License
+ * @link	http://codeigniter.com
+ * @since	Version 1.0.0
+ * @filesource
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['email_must_be_array'] = 'Passed an array, the email validation method must be.  Yes, hmmm.';
+$lang['email_invalid_address'] = 'Invalid email address: %s';
+$lang['email_attachment_missing'] = 'To locate the following email attachment unable.  : %s';
+$lang['email_attachment_unreadable'] = 'To locate the following email attachment unable.  Yes, hmmm.: %s';
+$lang['email_no_from'] = 'Send mail with no "From"  Header, I cannot.  Yeesssssss.';
+$lang['email_no_recipients'] = 'Recipients (To, Cc, or Bcc) must you include ';
+$lang['email_send_failure_phpmail'] = 'To send email using PHP mail() unable.  Your server might be configured to send mail using this method not.  Hmmmmmm.';
+$lang['email_send_failure_sendmail'] = 'To send email using PHP sendmail unable.  Your server might be configured to send mail using this method not.  Hmmmmmm.';
+$lang['email_send_failure_smtp'] = 'To send email using PHP SMTP unable.  Your server might be configured to send mail using this method not.  Hmmmmmm.';
+$lang['email_sent'] = 'Been successfully sent using the following protocol, your message has.  Yes, hmmm.: %s';
+$lang['email_no_socket'] = 'To open a socket to sendmail unable.  Check settings.  Yes, hmmm.';
+$lang['email_no_hostname'] = 'Specify a smtp hostname, you did not.  ';
+$lang['email_smtp_error'] = 'Encountered, the following SMTP error was: %s';
+$lang['email_no_smtp_unpw'] = 'Error: a SMTP username and password assign you must.';
+$lang['email_failed_smtp_login'] = 'To send, AUTH LOGIN command failed. Error: %s';
+$lang['email_smtp_auth_un'] = 'Authenticate username, failed it did. Error: %s';
+$lang['email_smtp_auth_pw'] = 'Authenticate password, failed it did. Error: %s';
+$lang['email_smtp_data_failure'] = 'To send data, unable am I: %s';
+$lang['email_exit_status'] = 'Exit status code: %s. Yesss.';
+
+/* End of file email_lang.php */
+/* Location: ./application/language/english/email_lang.php */
---
+++ language/persian/imglib_lang.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * CodeIgniter
+ *
+ * An open source application development framework for PHP 5.2.4 or newer
+ *
+ * This content is released under the MIT License (MIT)
+ *
+ * Copyright (c) 2014, British Columbia Institute of Technology
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @package	CodeIgniter
+ * @author	EllisLab Dev Team
+ * @copyright	Copyright (c) 2008 - 2014, EllisLab, Inc. (http://ellislab.com/)
+ * @copyright	Copyright (c) 2014, British Columbia Institute of Technology (http://bcit.ca/)
+ * @license	http://opensource.org/licenses/MIT	MIT License
+ * @link	http://codeigniter.com
+ * @since	Version 1.0.0
+ * @filesource
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['imglib_source_image_required'] = 'Specify a source image in your preferences, you must.  Herh herh herh..';
+$lang['imglib_gd_required'] = 'The GD image library for this feature required is.';
+$lang['imglib_gd_required_for_props'] = 'The GD image library in order to determine the image properties, must your server support.';
+$lang['imglib_unsupported_imagecreate'] = 'The GD function required to process this type of image, your server supports it not.';
+$lang['imglib_gif_not_supported'] = 'GIF images supported due to licensing restrictions they are often not . Use JPG or PNG images instead, you might have to.';
+$lang['imglib_jpg_not_supported'] = 'JPG images not supported they are.';
+$lang['imglib_png_not_supported'] = 'PNG images not supported they are.';
+$lang['imglib_jpg_or_png_required'] = 'The image resize protocol specified in your preferences, with JPEG or PNG image types works it only.';
+$lang['imglib_copy_error'] = 'While attempting to replace the file, encountered an error was. Your file directory writable must it be.';
+$lang['imglib_rotate_unsupported'] = 'Image rotation your server does not appear to support.';
+$lang['imglib_libpath_invalid'] = 'The path to your image library correct it is not. The correct path in your image preferences set must be. Yessss.';
+$lang['imglib_image_process_failed'] = 'Failed is the image processing. Verify that your server supports the chosen protocol, and that the path to your image library is correct, you must.';
+$lang['imglib_rotation_angle_required'] = 'To rotate the image, an angle of rotation is required.';
+$lang['imglib_writing_failed_gif'] = 'GIF image.';
+$lang['imglib_invalid_path'] = 'The path to the image correct is not. Hmmmmm.';
+$lang['imglib_copy_failed'] = 'Failed, the image copy routine.';
+$lang['imglib_missing_font'] = 'A font to use unable to be found.';
+$lang['imglib_save_failed'] = 'The image saved it could not. Make sure the image and file directory are writable, you must.';
+
+/* End of file imglib_lang.php */
+/* Location: ./application/language/english/imglib_lang.php */
