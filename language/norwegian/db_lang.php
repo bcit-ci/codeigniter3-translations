44ddb69873083ac22b868bc993c8094f75e724a3
Yngve Høiseth <yngve.hoiseth@gmail.com>
2014-12-04 18:36:45 +0100

    Translated to Norwegian

--- language/norwegian/db_lang.php
+++ language/norwegian/db_lang.php
@@ -37,30 +37,30 @@
  */
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$lang['db_invalid_connection_str'] = 'Unable to determine the database settings based on the connection string you submitted.';
-$lang['db_unable_to_connect'] = 'Unable to connect to your database server using the provided settings.';
-$lang['db_unable_to_select'] = 'Unable to select the specified database: %s';
-$lang['db_unable_to_create'] = 'Unable to create the specified database: %s';
-$lang['db_invalid_query'] = 'The query you submitted is not valid.';
-$lang['db_must_set_table'] = 'You must set the database table to be used with your query.';
-$lang['db_must_use_set'] = 'You must use the "set" method to update an entry.';
-$lang['db_must_use_index'] = 'You must specify an index to match on for batch updates.';
-$lang['db_batch_missing_index'] = 'One or more rows submitted for batch updating is missing the specified index.';
-$lang['db_must_use_where'] = 'Updates are not allowed unless they contain a "where" clause.';
-$lang['db_del_must_use_where'] = 'Deletes are not allowed unless they contain a "where" or "like" clause.';
-$lang['db_field_param_missing'] = 'To fetch fields requires the name of the table as a parameter.';
-$lang['db_unsupported_function'] = 'This feature is not available for the database you are using.';
-$lang['db_transaction_failure'] = 'Transaction failure: Rollback performed.';
-$lang['db_unable_to_drop'] = 'Unable to drop the specified database.';
-$lang['db_unsupported_feature'] = 'Unsupported feature of the database platform you are using.';
-$lang['db_unsupported_compression'] = 'The file compression format you chose is not supported by your server.';
-$lang['db_filepath_error'] = 'Unable to write data to the file path you have submitted.';
-$lang['db_invalid_cache_path'] = 'The cache path you submitted is not valid or writable.';
-$lang['db_table_name_required'] = 'A table name is required for that operation.';
-$lang['db_column_name_required'] = 'A column name is required for that operation.';
-$lang['db_column_definition_required'] = 'A column definition is required for that operation.';
-$lang['db_unable_to_set_charset'] = 'Unable to set client connection character set: %s';
-$lang['db_error_heading'] = 'A Database Error Occurred';
+$lang['db_invalid_connection_str'] = 'Ikke i stand til å fastsette tilkoblingen til databasen basert på tilkoblingsteksten du oppgav.';
+$lang['db_unable_to_connect'] = 'Ikke i stand til å koble til databasetjeneren din ved hjelp av de oppgitte innstillingene.';
+$lang['db_unable_to_select'] = 'Ikke i stand til å velge den spesifiserte databasen: %s';
+$lang['db_unable_to_create'] = 'Ikke i stand til å lage den spesifiserte databasen: %s';
+$lang['db_invalid_query'] = 'Foresåpørselen du sendte inn er ikke gyldig.';
+$lang['db_must_set_table'] = 'Du må spesifisere hvilken database-tabell som skal brukes med forespørselen din.';
+$lang['db_must_use_set'] = 'Du må bruke «set»-metoden for å oppdatere en oppføring.';
+$lang['db_must_use_index'] = 'Du må spesifisere en indeks som det skal passe med ved multi-oppdateringer.';
+$lang['db_batch_missing_index'] = 'En eller flere rader som er sendt inn for multi-oppdatering mangler den spesifiserte indeksen.';
+$lang['db_must_use_where'] = 'Oppdateringer uten «where»-klausul er ikke tillatt.';
+$lang['db_del_must_use_where'] = 'Fjerninger uten en «where»- eller «like»-klausul er ikke tillatt.';
+$lang['db_field_param_missing'] = 'Tabellnavnet må oppgis som parameter for å hente felter.';
+$lang['db_unsupported_function'] = 'Denne funksjonen er ikke tilgjengelig med databasen du bruker.';
+$lang['db_transaction_failure'] = 'Transaksjonsfeil: Tilbakerulling utført.';
+$lang['db_unable_to_drop'] = 'Ikke i stand til å fjerne den valgte databasen.';
+$lang['db_unsupported_feature'] = 'Databaseplattformen du bruker støtter ikke denne funksjonen.';
+$lang['db_unsupported_compression'] = 'Filkomprimeringsformatet du valgte støttes ikke av tjeneren din.';
+$lang['db_filepath_error'] = 'Skriving av data til den valgte filstien mislyktes.';
+$lang['db_invalid_cache_path'] = 'Cache-stien du valgte er enten ugyldig eller ikke skrivbar.';
+$lang['db_table_name_required'] = 'Et tabellnavn er påkrevet for å gjennomføre denne operasjonen.';
+$lang['db_column_name_required'] = 'Et kolonnenavn er påkrevet for å gjennomføre denne operasjonen.';
+$lang['db_column_definition_required'] = 'En kolonnedefinisjon er påkrevet for å gjennomføre denne operasjonen.';
+$lang['db_unable_to_set_charset'] = 'Definering av klient-tegnsett mislyktes: %s';
+$lang['db_error_heading'] = 'En databasefeil oppstod';
 
 /* End of file db_lang.php */
 /* Location: ./system/language/english/db_lang.php */
