15a476cb62a58d122333a6e1265259ae1487eda5
Yngve Høiseth <yngve.hoiseth@gmail.com>
2014-12-04 18:45:53 +0100

    Translated to Norwegian

--- language/norwegian/email_lang.php
+++ language/norwegian/email_lang.php
@@ -37,25 +37,25 @@
  */
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$lang['email_must_be_array'] = 'The email validation method must be passed an array.';
-$lang['email_invalid_address'] = 'Invalid email address: %s';
-$lang['email_attachment_missing'] = 'Unable to locate the following email attachment: %s';
-$lang['email_attachment_unreadable'] = 'Unable to open this attachment: %s';
-$lang['email_no_from'] = 'Cannot send mail with no "From" header.';
-$lang['email_no_recipients'] = 'You must include recipients: To, Cc, or Bcc';
-$lang['email_send_failure_phpmail'] = 'Unable to send email using PHP mail(). Your server might not be configured to send mail using this method.';
-$lang['email_send_failure_sendmail'] = 'Unable to send email using PHP Sendmail. Your server might not be configured to send mail using this method.';
-$lang['email_send_failure_smtp'] = 'Unable to send email using PHP SMTP. Your server might not be configured to send mail using this method.';
-$lang['email_sent'] = 'Your message has been successfully sent using the following protocol: %s';
-$lang['email_no_socket'] = 'Unable to open a socket to Sendmail. Please check settings.';
-$lang['email_no_hostname'] = 'You did not specify a SMTP hostname.';
-$lang['email_smtp_error'] = 'The following SMTP error was encountered: %s';
-$lang['email_no_smtp_unpw'] = 'Error: You must assign a SMTP username and password.';
-$lang['email_failed_smtp_login'] = 'Failed to send AUTH LOGIN command. Error: %s';
-$lang['email_smtp_auth_un'] = 'Failed to authenticate username. Error: %s';
-$lang['email_smtp_auth_pw'] = 'Failed to authenticate password. Error: %s';
-$lang['email_smtp_data_failure'] = 'Unable to send data: %s';
-$lang['email_exit_status'] = 'Exit status code: %s';
+$lang['email_must_be_array'] = 'E-postvalideringsmetoden krever en matrise.';
+$lang['email_invalid_address'] = 'Ugyldig e-postadresse: %s';
+$lang['email_attachment_missing'] = 'Fant ikke følgende e-postvedlegg: %s';
+$lang['email_attachment_unreadable'] = 'Klarte ikke å åpne dette vedlegget: %s';
+$lang['email_no_from'] = 'Kan ikke sende e-post uten «From»-header.';
+$lang['email_no_recipients'] = 'Du må oppgi mottakere: til, kopi eller blindkopi.';
+$lang['email_send_failure_phpmail'] = 'Klarte ikke å sende e-post ved hjelp av PHP mail(). Tjeneren din er kanskje ikke konfigurert til å sende e-post ved hjelp av denne metoden.';
+$lang['email_send_failure_sendmail'] = 'Klarte ikke å sende e-post ved hjelp av PHP Sendmail. Tjeneren din er kanskje ikke konfigurert til å sende e-post ved hjelp av denne metoden.';
+$lang['email_send_failure_smtp'] = 'Klarte ikke å sende e-post ved hjelp av PHP SMTP. Tjeneren din er kanskje ikke konfigurert til å sende e-post ved hjelp av denne metoden.';
+$lang['email_sent'] = 'Meldingen din er sendt ved hjelp av følgende protokoll: %s';
+$lang['email_no_socket'] = 'Klarte ikke å oppnå kontakt med Sendmail. Vennligst sjekk innstillingene dine.';
+$lang['email_no_hostname'] = 'Du oppgav ikke noe vertsnavn for SMTP.';
+$lang['email_smtp_error'] = 'Følgende SMTP-feil oppstod: %s';
+$lang['email_no_smtp_unpw'] = 'Feil: Du må oppgi SMTP-brukernavn og -passord.';
+$lang['email_failed_smtp_login'] = 'AUTH LOGIN-kommandoen ble ikke sendt. Feil: %s';
+$lang['email_smtp_auth_un'] = 'Feil ved autentisering av brukernavn: %s';
+$lang['email_smtp_auth_pw'] = 'Feil ved autentisering av passord: %s';
+$lang['email_smtp_data_failure'] = 'Klarte ikke å sende data: %s';
+$lang['email_exit_status'] = 'Statuskode ved avslutning: %s';
 
 /* End of file email_lang.php */
-/* Location: ./system/language/english/email_lang.php */
+/* Location: ./system/language/norwegian/email_lang.php */
