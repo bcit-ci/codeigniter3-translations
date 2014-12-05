d10ea831462d0f2b5e99dec6b98520b906434153
Mateus Brandao <mateusfbi@gmail.com>
2014-12-05 09:18:00 -0300

    Add portuguese-brazilian

---
+++ language/portuguese-brazilian/upload_lang.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * CodeIgniter
+ *
+ * An open source application development framework for PHP 5.2.4 or newer
+ *
+ * This content is released under the MIT License (MIT)
+ *
+ * Copyright (c) 2014, British Columbia Institute of Technology
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @package	CodeIgniter
+ * @author	EllisLab Dev Team
+ * @copyright	Copyright (c) 2008 - 2014, EllisLab, Inc. (http://ellislab.com/)
+ * @copyright	Copyright (c) 2014, British Columbia Institute of Technology (http://bcit.ca/)
+ * @license	http://opensource.org/licenses/MIT	MIT License
+ * @link	http://codeigniter.com
+ * @since	Version 1.0.0
+ * @filesource
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['upload_userfile_not_set'] = 'Não foi possível encontrar a variável post chamada userfile.';
+$lang['upload_file_exceeds_limit'] = 'O arquivo excedeu o tamanho máximo permitido de upload em seu arquivo de configuração do PHP.';
+$lang['upload_file_exceeds_form_limit'] = 'O arquivo excedeu o tamanho de upload permitido no envio do seu formulário.';
+$lang['upload_file_partial'] = 'O arquivo foi apenas parcialmente carregado.  .';
+$lang['upload_no_temp_directory'] = 'Está faltando a pasta temporária.';
+$lang['upload_unable_to_write_file'] = 'O arquivo não pode ser escrito no disco..';
+$lang['upload_stopped_by_extension'] = 'O upload de arquivo foi parado por causa de sua extensão.';
+$lang['upload_no_file_selected'] = 'Você não selecionou o arquivo para fazer upload.';
+$lang['upload_invalid_filetype'] = 'O tipo de arquivo que você está tentando fazer upload não é permitido.';
+$lang['upload_invalid_filesize'] = 'O arquivo que você está tentando fazer upload é maior que o tamanho permitido.';
+$lang['upload_invalid_dimensions'] = 'A imagem que você está tentando fazer upload excede as dimensões máxima permitida.';
+$lang['upload_destination_error'] = 'Um problema foi encontrado enquanto tentava mover o arquivo que foi feito upload para o destino final.';
+$lang['upload_no_filepath'] = 'O caminho(path) para upload parece não ser válido.';
+$lang['upload_no_file_types'] = 'Você não especificou o tipo de arquivo permitido';
+$lang['upload_bad_filename'] = 'O nome do arquivo que você enviou já existe em nosso servidor..';
+$lang['upload_not_writable'] = 'A pasta destino para upload parece não ter permissão para gravar.';
+
+/* End of file upload_lang.php */
+/* Location: ./application/language/portuguese-brazilian/upload_lang.php */
