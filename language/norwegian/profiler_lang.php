2e313a26a788145d2b98f596722299b45ebd6a2b
Yngve Høiseth <yngve.hoiseth@gmail.com>
2014-12-08 22:40:00 +0100

    Translated to Norwegian

--- language/norwegian/profiler_lang.php
+++ language/norwegian/profiler_lang.php
@@ -38,26 +38,26 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 $lang['profiler_database']		= 'DATABASE';
-$lang['profiler_controller_info'] = 'CLASS/METHOD';
-$lang['profiler_benchmarks']	= 'BENCHMARKS';
-$lang['profiler_queries']		= 'QUERIES';
-$lang['profiler_get_data']		= 'GET DATA';
-$lang['profiler_post_data']		= 'POST DATA';
-$lang['profiler_uri_string']	= 'URI STRING';
-$lang['profiler_memory_usage']	= 'MEMORY USAGE';
-$lang['profiler_config']		= 'CONFIG VARIABLES';
-$lang['profiler_session_data']	= 'SESSION DATA';
-$lang['profiler_headers']		= 'HTTP HEADERS';
-$lang['profiler_no_db']			= 'Database driver is not currently loaded';
-$lang['profiler_no_queries']	= 'No queries were run';
-$lang['profiler_no_post']		= 'No POST data exists';
-$lang['profiler_no_get']		= 'No GET data exists';
-$lang['profiler_no_uri']		= 'No URI data exists';
-$lang['profiler_no_memory']		= 'Memory Usage Unavailable';
-$lang['profiler_no_profiles']	= 'No Profile data - all Profiler sections have been disabled.';
-$lang['profiler_section_hide']	= 'Hide';
-$lang['profiler_section_show']	= 'Show';
-$lang['profiler_seconds']		= 'seconds';
+$lang['profiler_controller_info'] = 'KLASSE/METODE';
+$lang['profiler_benchmarks']	= 'YTELSESMÅLINGER';
+$lang['profiler_queries']		= 'FORESPØRSLER';
+$lang['profiler_get_data']		= 'GET-DATA';
+$lang['profiler_post_data']		= 'POST-DATA';
+$lang['profiler_uri_string']	= 'URI-STRENG';
+$lang['profiler_memory_usage']	= 'MINNEBRUK';
+$lang['profiler_config']		= 'KONFIGURASJONSVARIABLER';
+$lang['profiler_session_data']	= 'SESSION-DATA';
+$lang['profiler_headers']		= 'HTTP-HEADERE';
+$lang['profiler_no_db']			= 'Database-driveren er ikke lastet for øyeblikket';
+$lang['profiler_no_queries']	= 'Ingen forespørsler ble kjørt';
+$lang['profiler_no_post']		= 'Ingen POST-data eksisterer';
+$lang['profiler_no_get']		= 'ingen GET-data eksisterer';
+$lang['profiler_no_uri']		= 'Ingen URI-data eksisterer';
+$lang['profiler_no_memory']		= 'Minnebruk ikke tilgjengelig';
+$lang['profiler_no_profiles']	= 'Ingen profildata - alle profiler-seksjoner har blitt deaktivert.';
+$lang['profiler_section_hide']	= 'Skjul';
+$lang['profiler_section_show']	= 'Vis';
+$lang['profiler_seconds']		= 'Sekunder';
 
 /* End of file profiler_lang.php */
-/* Location: ./system/language/english/profiler_lang.php */
+/* Location: ./system/language/norwegian/profiler_lang.php */
