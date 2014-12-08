562c71b9edadf5e4c640b3cc68d437b9fe1a630e
Yngve Høiseth <yngve.hoiseth@gmail.com>
2014-12-08 21:15:55 +0100

    translated to norwegian

--- language/norwegian/ftp_lang.php
+++ language/norwegian/ftp_lang.php
@@ -37,18 +37,19 @@
  */
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$lang['ftp_no_connection']		= 'Unable to locate a valid connection ID. Please make sure you are connected before performing any file routines.';
-$lang['ftp_unable_to_connect']		= 'Unable to connect to your FTP server using the supplied hostname.';
-$lang['ftp_unable_to_login']		= 'Unable to login to your FTP server. Please check your username and password.';
+$lang['ftp_no_connection']		= 'Fant ingen gyldig tilkoblings-ID. Vennligst sørg for å være tilkoblet før du utfører filoperasjoner.';
+$lang['ftp_unable_to_connect']		= 'Klarte ikke å koble til FTP-tjeneren din med det oppgitte vertsnavnet.';
+$lang['ftp_unable_to_login']		= 'Klarte ikke å logge inn på FTP-tjeneren din. Vennligst sjekk om du oppgav rett brukernavn og passord.';
 $lang['ftp_unable_to_mkdir']		= 'Unable to create the directory you have specified.';
-$lang['ftp_unable_to_changedir']	= 'Unable to change directories.';
-$lang['ftp_unable_to_chmod']		= 'Unable to set file permissions. Please check your path.';
-$lang['ftp_unable_to_upload']		= 'Unable to upload the specified file. Please check your path.';
-$lang['ftp_unable_to_download']		= 'Unable to download the specified file. Please check your path.';
-$lang['ftp_no_source_file']		= 'Unable to locate the source file. Please check your path.';
-$lang['ftp_unable_to_rename']		= 'Unable to rename the file.';
-$lang['ftp_unable_to_delete']		= 'Unable to delete the file.';
-$lang['ftp_unable_to_move']		= 'Unable to move the file. Please make sure the destination directory exists.';
+$lang['ftp_unable_to_mkdir']		= 'Klarte ikke å opprette mappe.';
+$lang['ftp_unable_to_changedir']	= 'Klarte ikke å endre mappe.';
+$lang['ftp_unable_to_chmod']		= 'Klarte ikke å sette innstillinger for filtilgang. Vennligst sjekk filstien.';
+$lang['ftp_unable_to_upload']		= 'Klarte ikke å laste opp filen. Vennligst sjekk filstien.';
+$lang['ftp_unable_to_download']		= 'Klarte ikke å laste ned filen. Vennligst sjekk filstien.';
+$lang['ftp_no_source_file']		= 'Fant ikke kildefilen. Vennligst sjekk filstien.';
+$lang['ftp_unable_to_rename']		= 'Klarte ikke å gi filen nytt navn.';
+$lang['ftp_unable_to_delete']		= 'Klarte ikke å slette filen.';
+$lang['ftp_unable_to_move']		= 'Klarte ikke å flytte filen. Vennligst sjekk at mappen eksisterer.';
 
 /* End of file ftp_lang.php */
-/* Location: ./system/language/english/ftp_lang.php */
+/* Location: ./system/language/norwegian/ftp_lang.php */
