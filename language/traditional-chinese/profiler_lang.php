11a597c8c984b85b78c903fea4a8b9428fcb9fa2
Peter Dave Hello <hsu@peterdavehello.org>
2014-12-20 19:17:41 +0800

    add traditional-chinese

---
+++ language/traditional-chinese/profiler_lang.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * CodeIgniter
+ *
+ * An open source application development framework for PHP 5.2.4 or newer
+ *
+ * This content is released under the MIT License (MIT)
+ *
+ * Copyright (c) 2014, British Columbia Institute of Technology
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @package	CodeIgniter
+ * @author	EllisLab Dev Team
+ * @copyright	Copyright (c) 2008 - 2014, EllisLab, Inc. (http://ellislab.com/)
+ * @copyright	Copyright (c) 2014, British Columbia Institute of Technology (http://bcit.ca/)
+ * @license	http://opensource.org/licenses/MIT	MIT License
+ * @link	http://codeigniter.com
+ * @since	Version 1.0.0
+ * @filesource
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['profiler_database'] = '資料庫';
+$lang['profiler_controller_info'] = '類別 / 方法';
+$lang['profiler_benchmarks'] = '基準';
+$lang['profiler_queries'] = '查詢';
+$lang['profiler_get_data'] = 'GET 資料';
+$lang['profiler_post_data'] = 'POST 資料';
+$lang['profiler_uri_string'] = 'URI 字串';
+$lang['profiler_memory_usage'] = '記憶體使用';
+$lang['profiler_config'] = '設定值';
+$lang['profiler_session_data'] = 'SESSION 資料';
+$lang['profiler_headers'] = 'HTTP 標頭';
+$lang['profiler_no_db'] = '當前沒有已經載入的資料庫Driver。';
+$lang['profiler_no_queries'] = '沒有查詢。';
+$lang['profiler_no_post'] = 'POST 資料不存在。';
+$lang['profiler_no_get'] = 'GET 資料不存在。';
+$lang['profiler_no_uri'] = 'URI 資料不存在。';
+$lang['profiler_no_memory'] = '無法取得記憶體使用資料。';
+$lang['profiler_no_profiles'] = '沒有 Profile 資料 - 全部的 Profiler 都已被關閉。';
+$lang['profiler_section_hide'] = '隱藏';
+$lang['profiler_section_show'] = '顯示';
+$lang['profiler_seconds'] = '秒';
+
+/* End of file profiler_lang.php */
+/* Location: ./application/language/traditional-chinese/profiler_lang.php */
