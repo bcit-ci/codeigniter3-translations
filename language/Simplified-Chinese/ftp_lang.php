340ac24acb7253bcec4223bf896ee4b8e68b5fc5
denghongcai <admin@dhchouse.com>
2014-12-20 13:29:42 +0800

    Add Simplified-Chinese

---
+++ language/Simplified-Chinese/ftp_lang.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * CodeIgniter
+ *
+ * An open source application development framework for PHP 5.2.4 or newer
+ *
+ * This content is released under the MIT License (MIT)
+ *
+ * Copyright (c) 2014, British Columbia Institute of Technology
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @package	CodeIgniter
+ * @author	EllisLab Dev Team
+ * @copyright	Copyright (c) 2008 - 2014, EllisLab, Inc. (http://ellislab.com/)
+ * @copyright	Copyright (c) 2014, British Columbia Institute of Technology (http://bcit.ca/)
+ * @license	http://opensource.org/licenses/MIT	MIT License
+ * @link	http://codeigniter.com
+ * @since	Version 1.0.0
+ * @filesource
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['ftp_no_connection'] = '无法找到一个有效的连接 ID。在执行任何文件例程时请确保已经连接成功。';
+$lang['ftp_unable_to_connect'] = '无法使用提供的主机名连接到 FTP 服务器。';
+$lang['ftp_unable_to_login'] = '无法登录到 FTP 服务器，请检查用户名及密码是否正确。';
+$lang['ftp_unable_to_mkdir'] = '无法创建指定的木灵。';
+$lang['ftp_unable_to_changedir'] = '无法改变目录。';
+$lang['ftp_unable_to_chmod'] = '无法设置文件权限，检查文件路径或用户权限。';
+$lang['ftp_unable_to_upload'] = '无法上传指定的文件，检查上传路径或用户权限。';
+$lang['ftp_unable_to_download'] = '无法下载指定的文件，检查下载路径或用户权限。';
+$lang['ftp_no_source_file'] = '无法找到指定的源文件，检查文件路径或用户权限。';
+$lang['ftp_unable_to_rename'] = '无法重命名文件。';
+$lang['ftp_unable_to_delete'] = '无法删除文件。';
+$lang['ftp_unable_to_move'] = '无法移动文件，检查目的目录是否存在。';
+
+/* End of file ftp_lang.php */
+/* Location: ./application/language/english/ftp_lang.php */
---
+++ language/Simplified-Chinese/profiler_lang.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * CodeIgniter
+ *
+ * An open source application development framework for PHP 5.2.4 or newer
+ *
+ * This content is released under the MIT License (MIT)
+ *
+ * Copyright (c) 2014, British Columbia Institute of Technology
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @package	CodeIgniter
+ * @author	EllisLab Dev Team
+ * @copyright	Copyright (c) 2008 - 2014, EllisLab, Inc. (http://ellislab.com/)
+ * @copyright	Copyright (c) 2014, British Columbia Institute of Technology (http://bcit.ca/)
+ * @license	http://opensource.org/licenses/MIT	MIT License
+ * @link	http://codeigniter.com
+ * @since	Version 1.0.0
+ * @filesource
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['profiler_database'] = '数据库';
+$lang['profiler_controller_info'] = '类 / 方法';
+$lang['profiler_benchmarks'] = '基准';
+$lang['profiler_queries'] = '查询';
+$lang['profiler_get_data'] = 'GET 数据';
+$lang['profiler_post_data'] = 'POST 数据';
+$lang['profiler_uri_string'] = 'URI 字符串';
+$lang['profiler_memory_usage'] = '内存使用';
+$lang['profiler_config'] = '设置值';
+$lang['profiler_session_data'] = 'SESSION 数据';
+$lang['profiler_headers'] = 'HTTP 头';
+$lang['profiler_no_db'] = '当前没有已经载入的数据库驱动。  ';
+$lang['profiler_no_queries'] = '运行，无查询。  ';
+$lang['profiler_no_post'] = 'POST 数据不存在。';
+$lang['profiler_no_get'] = 'GET 数据不存在。';
+$lang['profiler_no_uri'] = 'URI 数据不存在。';
+$lang['profiler_no_memory'] = '无法获得内存使用数据。';
+$lang['profiler_no_profiles'] = '没有 Profile 数据 - 全部的 Profiler 都已被关闭。';
+$lang['profiler_section_hide'] = '隐藏';
+$lang['profiler_section_show'] = '显示';
+$lang['profiler_seconds'] = '秒';
+
+/* End of file profiler_lang.php */
+/* Location: ./application/language/english/profiler_lang.php */
