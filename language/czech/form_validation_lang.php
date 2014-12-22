573c779433c4d1e2bd92d40f7008a3b8f0d1606f
Štěpán Postránecký <stepan.postranecky@gmail.com>
2014-12-22 21:17:54 +0100

    form_validation translation

---
+++ language/czech/form_validation_lang.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * CodeIgniter
+ *
+ * An open source application development framework for PHP 5.2.4 or newer
+ *
+ * This content is released under the MIT License (MIT)
+ *
+ * Copyright (c) 2014, British Columbia Institute of Technology
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * Pole above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @package	CodeIgniter
+ * @author	EllisLab Dev Team
+ * @copyright	Copyright (c) 2008 - 2014, EllisLab, Inc. (http://ellislab.com/)
+ * @copyright	Copyright (c) 2014, British Columbia Institute of Technology (http://bcit.ca/)
+ * @license	http://opensource.org/licenses/MIT	MIT License
+ * @link	http://codeigniter.com
+ * @since	Version 1.0.0
+ * @filesource
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['form_validation_required']		= 'Pole {field} je povinné.';
+$lang['form_validation_isset']			= 'Pole {field} musí obsahovat hodnotu.';
+$lang['form_validation_valid_email']		= 'Pole {field} musí obsahovat validní emailovo adresu.';
+$lang['form_validation_valid_emails']		= 'Pole {field} musí obsahovat validní emailovo adresy.';
+$lang['form_validation_valid_url']		= 'Pole {field} musí obsahovat validní URL.';
+$lang['form_validation_valid_ip']		= 'Pole {field} musí obsahovat validní IP.';
+$lang['form_validation_min_length']		= 'Pole {field} musí obsahovat alespoň {param} znaků.';
+$lang['form_validation_max_length']		= 'Pole {field} nesmí obsahovat více než {param} znaků.';
+$lang['form_validation_exact_length']		= 'Pole {field} musí obsahovat přesně {param} znaků.';
+$lang['form_validation_alpha']			= 'Pole {field} může obsahovat pouze znaky abecedy.';
+$lang['form_validation_alpha_numeric']		= 'Pole {field} může obsahovat pouze znaky abecedy a čísla.';
+$lang['form_validation_alpha_numeric_spaces']	= 'Pole {field} může obsahovat pouze znaky abecedy, čísla a mezery.';
+$lang['form_validation_alpha_dash']		= 'Pole {field} může obsahovat pouze znaky abecedy, čísla, podtržítka a pomlčky.';
+$lang['form_validation_numeric']		= 'Pole {field} může obsahovat pouze čísla.';
+$lang['form_validation_is_numeric']		= 'Pole {field} může obsahovat pouze číselné znaky.';
+$lang['form_validation_integer']		= 'Pole {field} musí být celé číslo.';
+$lang['form_validation_regex_match']		= 'Pole {field} není ve správném formátu.';
+$lang['form_validation_matches']		= 'Pole {field} není shodné s polem {param}.';
+$lang['form_validation_differs']		= 'Pole {field} musí být různé od pole {param}.';
+$lang['form_validation_is_unique'] 		= 'Pole {field} musí obsahovat unikátní hodnotu.';
+$lang['form_validation_is_natural']		= 'Pole {field} může obsahovat pouze přirozená čísla a nulu.';
+$lang['form_validation_is_natural_no_zero']	= 'Pole {field} může obsahovat pouze přirozená čísla.';
+$lang['form_validation_decimal']		= 'Pole {field} musí obsahovat desetinné čislo.';
+$lang['form_validation_less_than']		= 'Pole {field} musí být menší než pole {param}.';
+$lang['form_validation_less_than_equal_to']	= 'Pole {field} musí být menší nebo stejné jako pole {param}.';
+$lang['form_validation_greater_than']		= 'Pole {field} musí být větší než pole {param}.';
+$lang['form_validation_greater_than_equal_to']	= 'Pole {field} musí být větší nebo stejné jako pole {param}.';
+
+/* End of file form_validation_lang.php */
+/* Location: ./system/language/english/form_validation_lang.php */
