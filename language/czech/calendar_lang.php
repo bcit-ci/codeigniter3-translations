c229263231fb14e109aa99e20bc324ec86218a2e
Štěpán Postránecký <stepan.postranecky@gmail.com>
2014-12-22 19:59:58 +0100

    Calendar translation

---
+++ language/czech/calendar_lang.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * CodeIgniter
+ *
+ * An open source application development framework for PHP 5.2.4 or newer
+ *
+ * This content is released under the MIT License (MIT)
+ *
+ * Copyright (c) 2014, British Columbia Institute of Technology
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @package	CodeIgniter
+ * @author	EllisLab Dev Team
+ * @copyright	Copyright (c) 2008 - 2014, EllisLab, Inc. (http://ellislab.com/)
+ * @copyright	Copyright (c) 2014, British Columbia Institute of Technology (http://bcit.ca/)
+ * @license	http://opensource.org/licenses/MIT	MIT License
+ * @link	http://codeigniter.com
+ * @since	Version 1.0.0
+ * @filesource
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['cal_su']			= 'Ne';
+$lang['cal_mo']			= 'Po';
+$lang['cal_tu']			= 'Út';
+$lang['cal_we']			= 'St';
+$lang['cal_th']			= 'Čt';
+$lang['cal_fr']			= 'Pá';
+$lang['cal_sa']			= 'So';
+$lang['cal_sun']		= 'Ne';
+$lang['cal_mon']		= 'Po';
+$lang['cal_tue']		= 'Út';
+$lang['cal_wed']		= 'St';
+$lang['cal_thu']		= 'Čt';
+$lang['cal_fri']		= 'Pá';
+$lang['cal_sat']		= 'So';
+$lang['cal_sunday']		= 'Neděle';
+$lang['cal_monday']		= 'Pondělí';
+$lang['cal_tuesday']	= 'Úterý';
+$lang['cal_wednesday']	= 'Středa';
+$lang['cal_thursday']	= 'Čtvrtek';
+$lang['cal_friday']		= 'Pátek';
+$lang['cal_saturday']	= 'Sobota';
+$lang['cal_jan']		= 'Led';
+$lang['cal_feb']		= 'Ún';
+$lang['cal_mar']		= 'Bře';
+$lang['cal_apr']		= 'Dub';
+$lang['cal_may']		= 'Kvě';
+$lang['cal_jun']		= 'Čer';
+$lang['cal_jul']		= 'Čvnc';
+$lang['cal_aug']		= 'Srp';
+$lang['cal_sep']		= 'Zář';
+$lang['cal_oct']		= 'Říj';
+$lang['cal_nov']		= 'Lis';
+$lang['cal_dec']		= 'Pro';
+$lang['cal_january']	= 'Leden';
+$lang['cal_february']	= 'Únor';
+$lang['cal_march']		= 'Březen';
+$lang['cal_april']		= 'Duben';
+$lang['cal_mayl']		= 'Květen';
+$lang['cal_june']		= 'Červen';
+$lang['cal_july']		= 'Červenec';
+$lang['cal_august']		= 'Srpen';
+$lang['cal_september']	= 'Září';
+$lang['cal_october']	= 'Říjen';
+$lang['cal_november']	= 'Listopad';
+$lang['cal_december']	= 'Prosinec';
+
+/* End of file calendar_lang.php */
+/* Location: ./system/language/english/calendar_lang.php */
