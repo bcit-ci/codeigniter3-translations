cbe93093952e7ffda69e77ef391233835fc906ec
Štěpán Postránecký <stepan.postranecky@gmail.com>
2014-12-22 21:37:53 +0100

    imglib_lang translation

---
+++ language/czech/imglib_lang.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * CodeIgniter
+ *
+ * An open source application development framework for PHP 5.2.4 or newer
+ *
+ * This content is released under the MIT License (MIT)
+ *
+ * Copyright (c) 2014, British Columbia Institute of Technology
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @package	CodeIgniter
+ * @author	EllisLab Dev Team
+ * @copyright	Copyright (c) 2008 - 2014, EllisLab, Inc. (http://ellislab.com/)
+ * @copyright	Copyright (c) 2014, British Columbia Institute of Technology (http://bcit.ca/)
+ * @license	http://opensource.org/licenses/MIT	MIT License
+ * @link	http://codeigniter.com
+ * @since	Version 1.0.0
+ * @filesource
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['imglib_source_image_required'] = 'Musíte zvolit zdrojový obrázek.';
+$lang['imglib_gd_required'] = 'Pro tuto funkci je potřeba knihovna GD.';
+$lang['imglib_gd_required_for_props'] = 'Váš server musí podporovat GD knihovnu pro zjištění vlastností obrázku.';
+$lang['imglib_unsupported_imagecreate'] = 'Váš server nepodporuje funkce GD knihovny nutné pro tento typ obrázku.';
+$lang['imglib_gif_not_supported'] = 'GIF obrázky často nejsou z důvodu licencování podporovány. Zvažte prosím použití formátu JPG nebo PNG.';
+$lang['imglib_jpg_not_supported'] = 'JPG obrázky nejsou podporovány.';
+$lang['imglib_png_not_supported'] = 'PNG obrázky nejsou podporovány.';
+$lang['imglib_jpg_or_png_required'] = 'Změna velikosti obrázku je možná pouze pro formáty JPG a PNG';
+$lang['imglib_copy_error'] = 'Nastal error při pokusu o přepsání souboru. Zkontrolujte prosím oprávnění souboru.';
+$lang['imglib_rotate_unsupported'] = 'Vypadá to, že otočení obrázku není serverem podporováno.';
+$lang['imglib_libpath_invalid'] = 'Cesta ke grafické knihovně není nastavena správně.';
+$lang['imglib_image_process_failed'] = 'Zpracování obrázku bylo neúspěšné. Zkontrolujte zda server podporuje daný protokol a zda-li je cesta nastavena správně.';
+$lang['imglib_rotation_angle_required'] = 'Pro otočení obrázku je potřeba zadat úhel.';
+$lang['imglib_invalid_path'] = 'Cesta k obrázku není správná.';
+$lang['imglib_copy_failed'] = 'Kopírování obrázku se nezdařilo.';
+$lang['imglib_missing_font'] = 'Nepodařilo se nalézt daný font.';
+$lang['imglib_save_failed'] = 'Nepodařilo se uložit obrázek. Zkontrolujte zda-li je možné zapisovat do adresáře.';
+
+/* End of file imglib_lang.php */
+/* Location: ./system/language/english/imglib_lang.php */
