4559a3b0c587d8934cc23e74c30f717317147731
Štěpán Postránecký <stepan.postranecky@gmail.com>
2014-12-22 21:51:36 +0100

    profiler_lang translation

---
+++ language/czech/profiler_lang.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * CodeIgniter
+ *
+ * An open source application development framework for PHP 5.2.4 or newer
+ *
+ * This content is released under the MIT License (MIT)
+ *
+ * Copyright (c) 2014, British Columbia Institute of Technology
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @package	CodeIgniter
+ * @author	EllisLab Dev Team
+ * @copyright	Copyright (c) 2008 - 2014, EllisLab, Inc. (http://ellislab.com/)
+ * @copyright	Copyright (c) 2014, British Columbia Institute of Technology (http://bcit.ca/)
+ * @license	http://opensource.org/licenses/MIT	MIT License
+ * @link	http://codeigniter.com
+ * @since	Version 1.0.0
+ * @filesource
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['profiler_database']		= 'DATABASE';
+$lang['profiler_controller_info'] = 'CLASS/METHOD';
+$lang['profiler_benchmarks']	= 'BENCHMARKS';
+$lang['profiler_queries']		= 'QUERIES';
+$lang['profiler_get_data']		= 'GET DATA';
+$lang['profiler_post_data']		= 'POST DATA';
+$lang['profiler_uri_string']	= 'URI ŘETĚZEC';
+$lang['profiler_memory_usage']	= 'VYUŽITÍ PAMĚTI';
+$lang['profiler_config']		= 'PROMĚNNÉ NASTAVENÍ';
+$lang['profiler_session_data']	= 'SESSION DATA';
+$lang['profiler_headers']		= 'HTTP HEADERS';
+$lang['profiler_no_db']			= 'Ovladač databáze není správně načten';
+$lang['profiler_no_queries']	= 'Žádné dotazy neprovedeny';
+$lang['profiler_no_post']		= 'Neexistují žádná POST data';
+$lang['profiler_no_get']		= 'Neexistují žádná GET data';
+$lang['profiler_no_uri']		= 'Neexistují žádná URI data';
+$lang['profiler_no_memory']		= 'Nedostupné využití paměti';
+$lang['profiler_no_profiles']	= 'Žádná profilová data. Všechny sekce profileru zakázány.';
+$lang['profiler_section_hide']	= 'Skryj';
+$lang['profiler_section_show']	= 'Ukaž';
+$lang['profiler_seconds']		= 'sekund';
+
+/* End of file profiler_lang.php */
+/* Location: ./system/language/english/profiler_lang.php */
