9147bbfec7ce102f65024819815687d3fafbdaf1
POSTRS01 <POSTRS01@BRNOLPT-DEV29.DSG.DSGROOT.INT>
2014-12-23 07:51:27 +0100

    formatting PSR-2

--- language/czech/ftp_lang.php
+++ language/czech/ftp_lang.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * CodeIgniter
  *
@@ -37,18 +38,18 @@
  */
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$lang['ftp_no_connection']		= 'Nebylo možno nalézt ID spojení. Ujistěte se prosím, že jste navázali spojení před práci se soubory.';
-$lang['ftp_unable_to_connect']		= 'Nebylo možné se připojit s daným hostname.';
-$lang['ftp_unable_to_login']		= 'Nepodařilo se spojit s FTP serverem, zkontrolujte prosím přihlašovací jméno a heslo.';
-$lang['ftp_unable_to_mkdir']		= 'Nepodařilo se vytvořit adresář.';
-$lang['ftp_unable_to_changedir']	= 'Nepodařilo se změnit adresář.';
-$lang['ftp_unable_to_chmod']		= 'Nepodařilo se nastavit práva souboru. Zkontrolujte prosím cestu.';
-$lang['ftp_unable_to_upload']		= 'Nepodařilo se nahrát soubor. Zkontrolujte prosím cestu.';
-$lang['ftp_unable_to_download']		= 'Nepodařilo se stáhnout soubor. Zkontrolujte prosím cestu.';
-$lang['ftp_no_source_file']		= 'Nepodařilo se najít zdrojové soubory. Zkontrolujte prosím cestu.';
-$lang['ftp_unable_to_rename']		= 'Nepodařilo se přejmenovat soubor.';
-$lang['ftp_unable_to_delete']		= 'Nepodařilo se smazat soubor.';
-$lang['ftp_unable_to_move']		= 'Nepodařilo se přesunout soubor. Zkontolujte prosím,zda cílová složka existuje.';
+$lang['ftp_no_connection']       = 'Nebylo možno nalézt ID spojení. Ujistěte se prosím, že jste navázali spojení před práci se soubory.';
+$lang['ftp_unable_to_connect']   = 'Nebylo možné se připojit s daným hostname.';
+$lang['ftp_unable_to_login']     = 'Nepodařilo se spojit s FTP serverem, zkontrolujte prosím přihlašovací jméno a heslo.';
+$lang['ftp_unable_to_mkdir']     = 'Nepodařilo se vytvořit adresář.';
+$lang['ftp_unable_to_changedir'] = 'Nepodařilo se změnit adresář.';
+$lang['ftp_unable_to_chmod']     = 'Nepodařilo se nastavit práva souboru. Zkontrolujte prosím cestu.';
+$lang['ftp_unable_to_upload']    = 'Nepodařilo se nahrát soubor. Zkontrolujte prosím cestu.';
+$lang['ftp_unable_to_download']  = 'Nepodařilo se stáhnout soubor. Zkontrolujte prosím cestu.';
+$lang['ftp_no_source_file']      = 'Nepodařilo se najít zdrojové soubory. Zkontrolujte prosím cestu.';
+$lang['ftp_unable_to_rename']    = 'Nepodařilo se přejmenovat soubor.';
+$lang['ftp_unable_to_delete']    = 'Nepodařilo se smazat soubor.';
+$lang['ftp_unable_to_move']      = 'Nepodařilo se přesunout soubor. Zkontolujte prosím,zda cílová složka existuje.';
 
 /* End of file ftp_lang.php */
 /* Location: ./system/language/english/ftp_lang.php */
