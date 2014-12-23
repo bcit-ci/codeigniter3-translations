207a98ca2059d0613922f887ea1abf9da166bd63
POSTRS01 <POSTRS01@BRNOLPT-DEV29.DSG.DSGROOT.INT>
2014-12-23 07:34:58 +0100

    formatting PSR-2

--- language/czech/migration_lang.php
+++ language/czech/migration_lang.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * CodeIgniter
  *
@@ -37,14 +38,14 @@
  */
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$lang['migration_none_found']		= 'Migrace nebyla nalezena.';
-$lang['migration_not_found']		= 'Migrace s číslem této verze nebyla nalezena: %s.';
-$lang['migration_sequence_gap']		= 'V sekvenci se nachází mezera blízko: %s.';
-$lang['migration_multiple_version']	= 'Existuje více migrací se stejným číslem verze: %s.';
-$lang['migration_class_doesnt_exist']	= 'Třída pro migraci "%s" nebyla nalezena.';
-$lang['migration_missing_up_method']	= 'V migrační tříde "%s" chybí "up" metoda.';
-$lang['migration_missing_down_method']	= 'V migrační tříde "%s" chybí "down" metoda.';
-$lang['migration_invalid_filename']	= 'Migrace "%s" má chybné jméno.';
+$lang['migration_none_found']          = 'Migrace nebyla nalezena.';
+$lang['migration_not_found']           = 'Migrace s číslem této verze nebyla nalezena: %s.';
+$lang['migration_sequence_gap']        = 'V sekvenci se nachází mezera blízko: %s.';
+$lang['migration_multiple_version']    = 'Existuje více migrací se stejným číslem verze: %s.';
+$lang['migration_class_doesnt_exist']  = 'Třída pro migraci "%s" nebyla nalezena.';
+$lang['migration_missing_up_method']   = 'V migrační tříde "%s" chybí "up" metoda.';
+$lang['migration_missing_down_method'] = 'V migrační tříde "%s" chybí "down" metoda.';
+$lang['migration_invalid_filename']    = 'Migrace "%s" má chybné jméno.';
 
 /* End of file migration_lang.php */
 /* Location: ./system/language/english/migration_lang.php */
--- language/czech/unit_test_lang.php
+++ language/czech/unit_test_lang.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * CodeIgniter
  *
@@ -37,25 +38,25 @@
  */
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$lang['ut_test_name']		= 'Jméno Testu';
-$lang['ut_test_datatype']	= 'Datový typ testu';
-$lang['ut_res_datatype']	= 'očekávaný datový typ';
-$lang['ut_result']			= 'Výsledek';
-$lang['ut_undefined']		= 'Nespecifikované jméno testu';
-$lang['ut_file']			= 'Jméno souboru';
-$lang['ut_line']			= 'Číslo řádku';
-$lang['ut_passed']			= 'Prošlo';
-$lang['ut_failed']			= 'Neprošlo';
-$lang['ut_boolean']			= 'Boolean';
-$lang['ut_integer']			= 'Integer';
-$lang['ut_float']			= 'Float';
-$lang['ut_double']			= 'Float'; // can be the same as float
-$lang['ut_string']			= 'Řetězec';
-$lang['ut_array']			= 'Pole';
-$lang['ut_object']			= 'Objekt';
-$lang['ut_resource']		= 'Zdroj';
-$lang['ut_null']			= 'Null';
-$lang['ut_notes']			= 'Poznámky';
+$lang['ut_test_name']     = 'Jméno Testu';
+$lang['ut_test_datatype'] = 'Datový typ testu';
+$lang['ut_res_datatype']  = 'očekávaný datový typ';
+$lang['ut_result']        = 'Výsledek';
+$lang['ut_undefined']     = 'Nespecifikované jméno testu';
+$lang['ut_file']          = 'Jméno souboru';
+$lang['ut_line']          = 'Číslo řádku';
+$lang['ut_passed']        = 'Prošlo';
+$lang['ut_failed']        = 'Neprošlo';
+$lang['ut_boolean']       = 'Boolean';
+$lang['ut_integer']       = 'Integer';
+$lang['ut_float']         = 'Float';
+$lang['ut_double']        = 'Float'; // can be the same as float
+$lang['ut_string']        = 'Řetězec';
+$lang['ut_array']         = 'Pole';
+$lang['ut_object']        = 'Objekt';
+$lang['ut_resource']      = 'Zdroj';
+$lang['ut_null']          = 'Null';
+$lang['ut_notes']         = 'Poznámky';
 
 /* End of file unit_test_lang.php */
 /* Location: ./system/language/english/unit_test_lang.php */
