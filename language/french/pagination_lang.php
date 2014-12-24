0adaf34a800901cad43c300cd16e41b319f3e90a
Alain Rivest <includebeer@gmail.com>
2014-12-23 21:04:18 -0500

    fix

--- language/french/pagination_lang.php
+++ language/french/pagination_lang.php
@@ -39,8 +39,8 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 $lang['pagination_first_link'] = '&lsaquo; Première';
-$lang['pagination_next_link'] = '>';
-$lang['pagination_prev_link'] = '<';
+$lang['pagination_next_link'] = '&gt;';
+$lang['pagination_prev_link'] = '&lt;';
 $lang['pagination_last_link'] = 'Dernière &rsaquo;';
 
 /* End of file pagination_lang.php */
