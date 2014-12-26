f171c6ba3b904e166d24ba90f11159da5d60c44a
Peter Denk <peter@skypay.se>
2014-12-26 02:15:11 +0100

    Swedish translation.
    Signed-off-by: Peter Denk <peter@skypay.se>

---
+++ language/swedish/calendar_lang.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * CodeIgniter
+ *
+ * An open source application development framework for PHP 5.2.4 or newer
+ *
+ * This content is released under the MIT License (MIT)
+ *
+ * Copyright (c) 2014, British Columbia Institute of Technology
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @package	CodeIgniter
+ * @author	EllisLab Dev Team
+ * @copyright	Copyright (c) 2008 - 2014, EllisLab, Inc. (http://ellislab.com/)
+ * @copyright	Copyright (c) 2014, British Columbia Institute of Technology (http://bcit.ca/)
+ * @license	http://opensource.org/licenses/MIT	MIT License
+ * @link	http://codeigniter.com
+ * @since	Version 1.0.0
+ * @filesource
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['cal_su']		= 'sö';
+$lang['cal_mo']		= 'må';
+$lang['cal_tu']		= 'ti';
+$lang['cal_we']		= 'on';
+$lang['cal_th']		= 'to';
+$lang['cal_fr']		= 'fr';
+$lang['cal_sa']		= 'lö';
+$lang['cal_sun']	= 'sön';
+$lang['cal_mon']	= 'mån';
+$lang['cal_tue']	= 'tis';
+$lang['cal_wed']	= 'ons';
+$lang['cal_thu']	= 'tor';
+$lang['cal_fri']	= 'fre';
+$lang['cal_sat']	= 'lör';
+$lang['cal_sunday']	= 'söndag';
+$lang['cal_monday']	= 'måndag';
+$lang['cal_tuesday']	= 'tisdag';
+$lang['cal_wednesday']	= 'onsdag';
+$lang['cal_thursday']	= 'torsdag';
+$lang['cal_friday']	= 'fredag';
+$lang['cal_saturday']	= 'lördag';
+$lang['cal_jan']	= 'jan';
+$lang['cal_feb']	= 'feb';
+$lang['cal_mar']	= 'mar';
+$lang['cal_apr']	= 'apr';
+$lang['cal_may']	= 'maj';
+$lang['cal_jun']	= 'jun';
+$lang['cal_jul']	= 'jul';
+$lang['cal_aug']	= 'aug';
+$lang['cal_sep']	= 'sep';
+$lang['cal_oct']	= 'okt';
+$lang['cal_nov']	= 'nov';
+$lang['cal_dec']	= 'dec';
+$lang['cal_january']	= 'januari';
+$lang['cal_february']	= 'februari';
+$lang['cal_march']	= 'mars';
+$lang['cal_april']	= 'april';
+$lang['cal_mayl']	= 'maj';
+$lang['cal_june']	= 'juni';
+$lang['cal_july']	= 'juli';
+$lang['cal_august']	= 'augusti';
+$lang['cal_september']	= 'september';
+$lang['cal_october']	= 'oktober';
+$lang['cal_november']	= 'november';
+$lang['cal_december']	= 'december';
+
+/* End of file calendar_lang.php */
+/* Location: ./system/language/swedish/calendar_lang.php */
