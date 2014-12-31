7c47b501a25997b6a6e8ee7b5efd2feecabf1046
HyeongJoo Kwon <kwonhyeongjoo@gmail.com>
2014-12-31 19:48:35 +0900

    korean translation

---
+++ language/korean/migration_lang.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * CodeIgniter
+ *
+ * An open source application development framework for PHP 5.2.4 or newer
+ *
+ * This content is released under the MIT License (MIT)
+ *
+ * Copyright (c) 2014, British Columbia Institute of Technology
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @package	CodeIgniter
+ * @author	EllisLab Dev Team
+ * @copyright	Copyright (c) 2008 - 2014, EllisLab, Inc. (http://ellislab.com/)
+ * @copyright	Copyright (c) 2014, British Columbia Institute of Technology (http://bcit.ca/)
+ * @license	http://opensource.org/licenses/MIT	MIT License
+ * @link	http://codeigniter.com
+ * @since	Version 3.0.0
+ * @filesource
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['migration_none_found']		= '마이그레이션 파일을 찾을 수 없습니다.';
+$lang['migration_not_found']		= '마이그레이션 버전정보가 없습니다: %s.';
+$lang['migration_sequence_gap']		= '마이그레이션 시퀀스와 차이가 있습니다: %s.';
+$lang['migration_multiple_version']	= '복수의 버전정보가 있습니다.: %s.';
+$lang['migration_class_doesnt_exist']	= '마이그레이션 "%s" 클래스 파일을 찾을 수 없습니다.';
+$lang['migration_missing_up_method']	= '마이그레이션 "%s" 클래스의 "up" 메소드를 찾을 수 없습니다.';
+$lang['migration_missing_down_method']	= '마이그레이션 "%s" 클래스의 "down" 메소드를 찾을 수 없습니다.';
+$lang['migration_invalid_filename']	= '마이그레이션 "%s"은(는) 유효하지 않은 파일입니다.';
+
+/* End of file migration_lang.php */
+/* Location: ./system/language/korean/migration_lang.php */
