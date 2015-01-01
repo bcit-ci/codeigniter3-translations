8fb1dc62f95a1618de062136f7e49792aa024a6d
Osman Bozdağ <bozdag@hotmail.com>
2015-01-01 21:47:22 +0200

    form_validation_lang file translated

--- language/turkish/form_validation_lang.php
+++ language/turkish/form_validation_lang.php
@@ -37,33 +37,33 @@
  */
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$lang['form_validation_required']		= 'The {field} field is required.';
-$lang['form_validation_isset']			= 'The {field} field must have a value.';
-$lang['form_validation_valid_email']		= 'The {field} field must contain a valid email address.';
-$lang['form_validation_valid_emails']		= 'The {field} field must contain all valid email addresses.';
-$lang['form_validation_valid_url']		= 'The {field} field must contain a valid URL.';
-$lang['form_validation_valid_ip']		= 'The {field} field must contain a valid IP.';
-$lang['form_validation_min_length']		= 'The {field} field must be at least {param} characters in length.';
-$lang['form_validation_max_length']		= 'The {field} field cannot exceed {param} characters in length.';
-$lang['form_validation_exact_length']		= 'The {field} field must be exactly {param} characters in length.';
-$lang['form_validation_alpha']			= 'The {field} field may only contain alphabetical characters.';
-$lang['form_validation_alpha_numeric']		= 'The {field} field may only contain alpha-numeric characters.';
-$lang['form_validation_alpha_numeric_spaces']	= 'The {field} field may only contain alpha-numeric characters and spaces.';
-$lang['form_validation_alpha_dash']		= 'The {field} field may only contain alpha-numeric characters, underscores, and dashes.';
-$lang['form_validation_numeric']		= 'The {field} field must contain only numbers.';
-$lang['form_validation_is_numeric']		= 'The {field} field must contain only numeric characters.';
-$lang['form_validation_integer']		= 'The {field} field must contain an integer.';
-$lang['form_validation_regex_match']		= 'The {field} field is not in the correct format.';
-$lang['form_validation_matches']		= 'The {field} field does not match the {param} field.';
-$lang['form_validation_differs']		= 'The {field} field must differ from the {param} field.';
-$lang['form_validation_is_unique'] 		= 'The {field} field must contain a unique value.';
-$lang['form_validation_is_natural']		= 'The {field} field must only contain digits.';
-$lang['form_validation_is_natural_no_zero']	= 'The {field} field must only contain digits and must be greater than zero.';
-$lang['form_validation_decimal']		= 'The {field} field must contain a decimal number.';
-$lang['form_validation_less_than']		= 'The {field} field must contain a number less than {param}.';
-$lang['form_validation_less_than_equal_to']	= 'The {field} field must contain a number less than or equal to {param}.';
-$lang['form_validation_greater_than']		= 'The {field} field must contain a number greater than {param}.';
-$lang['form_validation_greater_than_equal_to']	= 'The {field} field must contain a number greater than or equal to {param}.';
+$lang['form_validation_required']		= '{field} alanı gerekli.';
+$lang['form_validation_isset']			= '{field} alanında bir değer olmalı.';
+$lang['form_validation_valid_email']		= '{field} alanı geçerli bir email adresi içermeli.';
+$lang['form_validation_valid_emails']		= '{field} alanı geçerli email adresleri içermeli.';
+$lang['form_validation_valid_url']		= '{field} alanı geçerli bir URL içermeli.';
+$lang['form_validation_valid_ip']		= '{field} alanı geçerli bir IP içermeli.';
+$lang['form_validation_min_length']		= '{field} alanı en az {param} karakter uzunluğunda olmalı.';
+$lang['form_validation_max_length']		= '{field} alanı {param} karakteri aşamaz.';
+$lang['form_validation_exact_length']		= '{field} alanı tam {param} karakter uzunluğunda olmalı.';
+$lang['form_validation_alpha']			= '{field} alanı yalnız alfabetik karakterler içerebilir.';
+$lang['form_validation_alpha_numeric']		= '{field} alanı yalnız alfabetik ve sayısal karakterler içerebilir.';
+$lang['form_validation_alpha_numeric_spaces']	= '{field} alanı yalnız alfabetik, sayısal karakterler ve boşluklar içerebilir.';
+$lang['form_validation_alpha_dash']		= '{field} alanı yalnız alfabetik, sayısal karakterler, alt çizgi ve tire işaretleri içerebilir.';
+$lang['form_validation_numeric']		= '{field} alanı yalnız sayı içerebilir.';
+$lang['form_validation_is_numeric']		= '{field} alanı yalnız sayısal karakterler içerebilir.';
+$lang['form_validation_integer']		= '{field} alanı bir tamsayı içerebilir';
+$lang['form_validation_regex_match']		= '{field} alanı doğru formatta değil.';
+$lang['form_validation_matches']		= '{field} alanı ile {param} alanı eşleşmiyor.';
+$lang['form_validation_differs']		= '{field} alanı {param} alanından farklı olmalı.';
+$lang['form_validation_is_unique'] 		= '{field} alanı eşsiz bir değer içermeli.';
+$lang['form_validation_is_natural']		= '{field} alanı yalnız rakamlar içerebilir.';
+$lang['form_validation_is_natural_no_zero']	= '{field} alanı yalnız rakamlar içermeli ve sıfırdan büyük olmalı.';
+$lang['form_validation_decimal']		= '{field} alanı bir onluk sayı içermeli.';
+$lang['form_validation_less_than']		= '{field} alanı {param} den küçük bir sayı içermeli.';
+$lang['form_validation_less_than_equal_to']	= '{field} alanı {param} den küçük veya eşit bir sayı içermeli.';
+$lang['form_validation_greater_than']		= '{field} alanı {param} den büyük bir sayı içermeli.';
+$lang['form_validation_greater_than_equal_to']	= '{field} alanı {param} den büyük veya eşit bir sayı içermeli.';
 
 /* End of file form_validation_lang.php */
 /* Location: ./system/language/english/form_validation_lang.php */
