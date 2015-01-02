8c055ce97d890d7b84d5c630887cc53e3a4eb227
Osman Bozdağ <bozdag@hotmail.com>
2015-01-02 07:27:21 +0200

    ftp_lang file translated

--- language/turkish/ftp_lang.php
+++ language/turkish/ftp_lang.php
@@ -38,17 +38,17 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 $lang['ftp_no_connection']			= 'Geçerli bir bağlantı ID si bulunamadı. Herhangi bir dosya işlemi yapmadan bağlantınızı kontrol ediniz.';
-$lang['ftp_unable_to_connect']	= 'Verilen sunucu adı ile FTP sunucuya bağlanılamadı.';
+$lang['ftp_unable_to_connect']		= 'Verilen sunucu adı ile FTP sunucuya bağlanılamadı.';
 $lang['ftp_unable_to_login']		= 'FTP sunucuya giriş yapılamadı. Lütfen kullanıcı adı ve şifreyi kontrol ediniz.';
 $lang['ftp_unable_to_mkdir']		= 'Belirtilen klasör oluşturulamadı.';
 $lang['ftp_unable_to_changedir']	= 'Klasör değiştirilemedi.';
 $lang['ftp_unable_to_chmod']		= 'Dosya izinleri değiştirilemedi. Lütfen yolu kontrol ediniz.';
 $lang['ftp_unable_to_upload']		= 'Dosya yüklenemedi. Lütfen yolu kontrol ediniz.';
-$lang['ftp_unable_to_download']	= 'Dosya indirilemedi. Lütfen yolu kontrol ediniz.';
-$lang['ftp_no_source_file']		= 'Kaynak dosya bulunamadı. Lütfen yolu kontrol ediniz.';
+$lang['ftp_unable_to_download']		= 'Dosya indirilemedi. Lütfen yolu kontrol ediniz.';
+$lang['ftp_no_source_file']			= 'Kaynak dosya bulunamadı. Lütfen yolu kontrol ediniz.';
 $lang['ftp_unable_to_rename']		= 'Dosya adı değiştirilemedi.';
 $lang['ftp_unable_to_delete']		= 'Dosya silinemedi.';
-$lang['ftp_unable_to_move']		= 'Dosya taşınamadı. Lütfen dosyanın taşınacağı klasörün varlığını kontrol ediniz.';
+$lang['ftp_unable_to_move']			= 'Dosya taşınamadı. Lütfen dosyanın taşınacağı klasörün varlığını kontrol ediniz.';
 
 /* End of file ftp_lang.php */
 /* Location: ./system/language/english/ftp_lang.php */
