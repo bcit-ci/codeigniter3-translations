8c19f31e3304c5f645c1264ef971db344517a75d
Osman Bozdağ <bozdag@hotmail.com>
2015-01-02 07:35:13 +0200

    migration_lang file translated

--- language/turkish/migration_lang.php
+++ language/turkish/migration_lang.php
@@ -37,14 +37,14 @@
  */
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$lang['migration_none_found']		= 'No migrations were found.';
-$lang['migration_not_found']		= 'No migration could be found with the version number: %s.';
-$lang['migration_sequence_gap']		= 'There is a gap in the migration sequence near version number: %s.';
-$lang['migration_multiple_version']	= 'There are multiple migrations with the same version number: %s.';
-$lang['migration_class_doesnt_exist']	= 'The migration class "%s" could not be found.';
-$lang['migration_missing_up_method']	= 'The migration class "%s" is missing an "up" method.';
-$lang['migration_missing_down_method']	= 'The migration class "%s" is missing a "down" method.';
-$lang['migration_invalid_filename']	= 'Migration "%s" has an invalid filename.';
+$lang['migration_none_found']			= 'Herhangi bir taşınma bulunamadı.';
+$lang['migration_not_found']			= 'Belirtilen sürüm numaralı taşınma bulunamadı: %s.';
+$lang['migration_sequence_gap']			= 'Şu sürüm numarasına yakın taşınma sıralamasında bir boşluk var: %s.';
+$lang['migration_multiple_version']		= 'Aynı sürüm numaralı birden çok taşınma var: %s.';
+$lang['migration_class_doesnt_exist']	= 'Taşınma sınıfı "%s" bulunamadı.';
+$lang['migration_missing_up_method']	= '"%s" taşınma sınıfında "up" metodu eksik.';
+$lang['migration_missing_down_method']	= '"%s" taşınma sınıfında "down" metodu eksik.';
+$lang['migration_invalid_filename']		= '"%s" taşınmasında geçersiz dosya adı.';
 
 /* End of file migration_lang.php */
 /* Location: ./system/language/english/migration_lang.php */
