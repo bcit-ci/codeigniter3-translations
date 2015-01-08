3efd101276b30924ba7ff7f4649bcb084b24f40f
Stefano Mazzega <stefano.mazzega@gmail.com>
2015-01-08 13:31:30 +0100

    change HTML encoding to UTF-8

--- language/italian/migration_lang.php
+++ language/italian/migration_lang.php
@@ -39,12 +39,12 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 $lang['migration_none_found'] = 'Non ci sono migrazioni disponibili.';
-$lang['migration_not_found'] = 'Non &egrave; stato possibile trovare la migrazione numero: %s.';
+$lang['migration_not_found'] = 'Non è stato possibile trovare la migrazione numero: %s.';
 $lang['migration_sequence_gap'] = 'C\'è un divario nella sequenza di migrazione nei pressi del numero di versione: %s.';
 $lang['migration_multiple_version'] = 'Ci sono differenti migrazioni con lo stesso numero di versione: %s.';
-$lang['migration_class_doesnt_exist'] = 'Non &egrave; possibile trovare la classe migrazione "%s".';
-$lang['migration_missing_up_method'] = 'La classe migrazione "%s" &egrave; sprovvista del metodo "up".';
-$lang['migration_missing_down_method'] = 'La classe migrazione "%s" &egrave; sprovvista del metodo "down".';
+$lang['migration_class_doesnt_exist'] = 'Non è possibile trovare la classe migrazione "%s".';
+$lang['migration_missing_up_method'] = 'La classe migrazione "%s" è sprovvista del metodo "up".';
+$lang['migration_missing_down_method'] = 'La classe migrazione "%s" è sprovvista del metodo "down".';
 $lang['migration_invalid_filename'] = 'La migrazione "%s" ha un nome file non valido.';
 
 /* End of file migration_lang.php */
