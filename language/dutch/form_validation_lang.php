97b92689fb301a3d1b7eb9e3dcd935bc0fb70e74
Heesung Ahn <ahn.heesung@gmail.com>
2015-01-17 15:48:08 -0500

    A Dutch translation of 'form_validation_error_message_not_set'. #78 Signed-off-by:Heesung Ahn <ahn.heesung@gmail.com>

--- language/dutch/form_validation_lang.php
+++ language/dutch/form_validation_lang.php
@@ -68,6 +68,6 @@ $lang['form_validation_less_than']		= 'Het {field}-veld dient een nummer kleiner
 $lang['form_validation_less_than_equal_to']	= 'Het {field}-veld dient een nummer kleiner of gelijk aan {param} te bevatten.';
 $lang['form_validation_greater_than']		= 'Het {field}-veld dient een nummer groter dan {param} te bevatten.';
 $lang['form_validation_greater_than_equal_to']	= 'Het {field}-veld dient een nummer groter dan, of gelijk aan {param} te bevatten.';
-
+$lang['form_validation_error_message_not_set']  = 'Het {field}-Geen toegang tot een foutmelding door die overeenkomt met uw veldnaam.';
 /* End of file form_validation_lang.php */
 /* Location: ./application/language/dutch/form_validation_lang.php */
