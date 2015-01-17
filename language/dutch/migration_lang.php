7a8ddc29ae681f8f4934f1fdd3843c1db7e35ea8
Heesung Ahn <ahn.heesung@gmail.com>
2015-01-17 13:36:26 -0500

    Update Dutch header comment Signed-off-by:Heesung Ahn <ahn.heesung@gmail.com>

--- language/dutch/migration_lang.php
+++ language/dutch/migration_lang.php
@@ -1,14 +1,12 @@
 <?php
-
 /**
  * CodeIgniter
  *
- * An open source application development framework for PHP 5.2.4 or newer
+ * An open source application development framework for PHP
  *
  * This content is released under the MIT License (MIT)
  *
- * Copyright (c) 2014, Pieter Krul
- * Copyright (c) 2014, British Columbia Institute of Technology
+ * Copyright (c) 2014 - 2015, British Columbia Institute of Technology
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -31,11 +29,15 @@
  * @package	CodeIgniter
  * @author	EllisLab Dev Team
  * @copyright	Copyright (c) 2008 - 2014, EllisLab, Inc. (http://ellislab.com/)
- * @copyright	Copyright (c) 2014, British Columbia Institute of Technology (http://bcit.ca/)
+ * @copyright	Copyright (c) 2014 - 2015, British Columbia Institute of Technology (http://bcit.ca/)
  * @license	http://opensource.org/licenses/MIT	MIT License
  * @link	http://codeigniter.com
- * @since	Version 3.0.0
+ * @since	Version 1.0.0
  * @filesource
+ */
+
+/*
+ * Original translation provided by Copyright (c) 2014, Pieter Krul
  */
 defined('BASEPATH') OR exit('Directe toegang tot scripts is niet toegestaan');
 
