4cab6930ce1fa749f5c0e599925663f3c2cf2eac
Mutasim Ridlo, S.Kom <ridho@mutasimridlo.com>
2015-01-23 05:40:22 +0700

    Update migration_lang.php

--- language/indonesian/migration_lang.php
+++ language/indonesian/migration_lang.php
@@ -45,6 +45,3 @@ $lang['migration_class_doesnt_exist']	= 'Kelas migrasi "%s" tidak dapat ditemuka
 $lang['migration_missing_up_method']	= 'Kelas migrasi "%s" kehilangan metode "up".';
 $lang['migration_missing_down_method']	= 'Kelas migrasi "%s" kehilangan metode "down".';
 $lang['migration_invalid_filename']	= 'Migrasi "%s" memiliki nama berkas yang tidak valid.';
-
-/* End of file migration_lang.php */
-/* Location: ./application/language/indonesian/migration_lang.php */
