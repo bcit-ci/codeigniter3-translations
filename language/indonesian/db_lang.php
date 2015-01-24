d2e8f0ef24db3490ff5430dbb858edc4a39cde02
Mutasim Ridlo, S.Kom <ridho@mutasimridlo.com>
2015-01-24 10:44:17 +0700

    Update db_lang.php

--- language/indonesian/db_lang.php
+++ language/indonesian/db_lang.php
@@ -39,8 +39,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 $lang['db_invalid_connection_str'] = 'Tidak dapat menentukan pengaturan basis data berdasarkan string koneksi yang Anda kirimkan.';
 $lang['db_unable_to_connect'] = 'Tidak dapat terhubung ke server basis data Anda menggunakan pengaturan yang disediakan.';
-$lang['db_unable_to_select'] = 'Tidak dapat memilih basis data tertentu: %s';
-$lang['db_unable_to_create'] = 'Tidak dapat membuat basis data tertentu: %s';
+$lang['db_unable_to_select'] = 'Tidak dapat memilih basis data yang ditentukan: %s';
+$lang['db_unable_to_create'] = 'Tidak dapat membuat basis data yang ditentukan: %s';
 $lang['db_invalid_query'] = 'Kueri yang Anda kirimkan tidak valid.';
 $lang['db_must_set_table'] = 'Anda harus mengatur tabel basis data yang akan digunakan dengan kueri Anda.';
 $lang['db_must_use_set'] = 'Anda harus menggunakan metode "set" untuk memperbarui entri.';
@@ -51,7 +51,7 @@ $lang['db_del_must_use_where'] = 'Menghapus tidak diperbolehkan kecuali mereka m
 $lang['db_field_param_missing'] = 'Untuk mengambil bidang membutuhkan nama tabel sebagai parameter.';
 $lang['db_unsupported_function'] = 'Fitur ini tidak tersedia untuk basis data yang Anda gunakan.';
 $lang['db_transaction_failure'] = 'Kegagalan transaksi: Gulingkan kembali dilakukan.';
-$lang['db_unable_to_drop'] = 'Tidak dapat menghapus basis data tertentu.';
+$lang['db_unable_to_drop'] = 'Tidak dapat menghapus basis data yang ditentukan.';
 $lang['db_unsupported_feature'] = 'Fitur yang tidak didukung platform basis data yang Anda gunakan.';
 $lang['db_unsupported_compression'] = 'Format kompresi berkas yang Anda pilih tidak didukung oleh server Anda.';
 $lang['db_filepath_error'] = 'Tidak dapat menulis data ke jalur berkas yang telah Anda kirimkan.';
@@ -59,5 +59,5 @@ $lang['db_invalid_cache_path'] = 'Jalur Cache yang Anda diajukan tidak sah atau 
 $lang['db_table_name_required'] = 'Sebuah nama tabel diperlukan untuk operasi tersebut.';
 $lang['db_column_name_required'] = 'Sebuah nama kolom diperlukan untuk operasi tersebut.';
 $lang['db_column_definition_required'] = 'Definisi kolom diperlukan untuk operasi tersebut.';
-$lang['db_unable_to_set_charset'] = 'Tidak dapat mengatur set karakter klien koneksi: %s';
+$lang['db_unable_to_set_charset'] = 'Tidak dapat mengatur set karakter koneksi klien: %s';
 $lang['db_error_heading'] = 'Sebuah Kesalahan Basis Data Terjadi';
