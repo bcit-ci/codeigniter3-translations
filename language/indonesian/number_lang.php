eacd4997e4941ed6848a0a413d41ab720572b2eb
Mutasim Ridlo, S.Kom <ridho@mutasimridlo.com>
2015-01-25 07:15:01 +0700

    Reformat

--- language/indonesian/number_lang.php
+++ language/indonesian/number_lang.php
@@ -10,8 +10,8 @@
  */
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$lang['terabyte_abbr'] 	= 'TB';
-$lang['gigabyte_abbr'] 	= 'GB';
-$lang['megabyte_abbr']	= 'MB';
-$lang['kilobyte_abbr'] 	= 'KB';
-$lang['bytes'] 			= 'Bytes';
+$lang['terabyte_abbr'] = 'TB';
+$lang['gigabyte_abbr'] = 'GB';
+$lang['megabyte_abbr'] = 'MB';
+$lang['kilobyte_abbr'] = 'KB';
+$lang['bytes'] = 'Bytes';
--- language/indonesian/pagination_lang.php
+++ language/indonesian/pagination_lang.php
@@ -10,7 +10,7 @@
  */
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$lang['pagination_first_link']	= '&lsaquo; Pertama';
-$lang['pagination_next_link']	= '&gt;';
-$lang['pagination_prev_link']	= '&lt;';
-$lang['pagination_last_link']	= 'Terakhir &rsaquo;';
+$lang['pagination_first_link'] = '&lsaquo; Pertama';
+$lang['pagination_next_link'] = '&gt;';
+$lang['pagination_prev_link'] = '&lt;';
+$lang['pagination_last_link'] = 'Terakhir &rsaquo;';
