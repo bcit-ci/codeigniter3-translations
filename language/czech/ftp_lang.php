c7166d9d94346ebdb5c5e767062b5c0b347d32a4
hauerland <lubomir@hauerland.com>
2015-02-07 14:13:29 +0100

    Update ftp_lang.php
    Typos corrected

--- language/czech/ftp_lang.php
+++ language/czech/ftp_lang.php
@@ -9,7 +9,7 @@
  */
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$lang['ftp_no_connection']       = 'Nebylo možno nalézt ID spojení. Ujistěte se prosím, že jste navázali spojení před práci se soubory.';
+$lang['ftp_no_connection']       = 'Nebylo možno nalézt ID spojení. Ujistěte se prosím, že jste navázali spojení před prací se soubory.';
 $lang['ftp_unable_to_connect']   = 'Nebylo možné se připojit s daným hostname.';
 $lang['ftp_unable_to_login']     = 'Nepodařilo se spojit s FTP serverem, zkontrolujte prosím přihlašovací jméno a heslo.';
 $lang['ftp_unable_to_mkdir']     = 'Nepodařilo se vytvořit adresář.';
@@ -20,4 +20,4 @@ $lang['ftp_unable_to_download']  = 'Nepodařilo se stáhnout soubor. Zkontrolujt
 $lang['ftp_no_source_file']      = 'Nepodařilo se najít zdrojové soubory. Zkontrolujte prosím cestu.';
 $lang['ftp_unable_to_rename']    = 'Nepodařilo se přejmenovat soubor.';
 $lang['ftp_unable_to_delete']    = 'Nepodařilo se smazat soubor.';
-$lang['ftp_unable_to_move']      = 'Nepodařilo se přesunout soubor. Zkontolujte prosím,zda cílová složka existuje.';
+$lang['ftp_unable_to_move']      = 'Nepodařilo se přesunout soubor. Zkontrolujte prosím, zda cílová složka existuje.';
