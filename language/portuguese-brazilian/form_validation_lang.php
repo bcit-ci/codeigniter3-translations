29aba3200b7fa653992da70ebdc33c4c8eb65712
Instructor, Computer Systems Technology <jim_parry@bcit.ca>
2015-02-22 11:36:12 -0800

    Update form_validation_lang.php

--- language/portuguese-brazilian/form_validation_lang.php
+++ language/portuguese-brazilian/form_validation_lang.php
@@ -37,4 +37,4 @@ $lang['form_validation_less_than_equal_to'] = 'O campo {field} deve conter um n�
 $lang['form_validation_greater_than'] = 'O campo {field} deve conter um número maior que {param}.';
 $lang['form_validation_greater_than_equal_to'] = 'O campo {field} deve conter um número maior ou igual que {param}.';
 $lang['form_validation_error_message_not_set'] = 'Não existe uma mensagem de erro para o campo com o nome {field}.';
-$lang['form_validation_in_list'] = 'O campo {field} deve ser um de: {param}.'; // translated
+$lang['form_validation_in_list'] = 'O campo {field} deve ser um de: {param}.'; 
