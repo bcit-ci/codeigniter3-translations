39acdf21a322c6aa91e6ad9e0b6738f71e2ee579
Edzer Padilla <dzerium@gmail.com>
2015-03-08 14:36:01 +0800

    translate imglib_lang

--- language/filipino/imglib_lang.php
+++ language/filipino/imglib_lang.php
@@ -37,20 +37,20 @@
  */
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$lang['imglib_source_image_required'] = 'You must specify a source image in your preferences.';
-$lang['imglib_gd_required'] = 'The GD image library is required for this feature.';
-$lang['imglib_gd_required_for_props'] = 'Your server must support the GD image library in order to determine the image properties.';
-$lang['imglib_unsupported_imagecreate'] = 'Your server does not support the GD function required to process this type of image.';
-$lang['imglib_gif_not_supported'] = 'GIF images are often not supported due to licensing restrictions. You may have to use JPG or PNG images instead.';
-$lang['imglib_jpg_not_supported'] = 'JPG images are not supported.';
-$lang['imglib_png_not_supported'] = 'PNG images are not supported.';
-$lang['imglib_jpg_or_png_required'] = 'The image resize protocol specified in your preferences only works with JPEG or PNG image types.';
-$lang['imglib_copy_error'] = 'An error was encountered while attempting to replace the file. Please make sure your file directory is writable.';
-$lang['imglib_rotate_unsupported'] = 'Image rotation does not appear to be supported by your server.';
-$lang['imglib_libpath_invalid'] = 'The path to your image library is not correct. Please set the correct path in your image preferences.';
-$lang['imglib_image_process_failed'] = 'Image processing failed. Please verify that your server supports the chosen protocol and that the path to your image library is correct.';
-$lang['imglib_rotation_angle_required'] = 'An angle of rotation is required to rotate the image.';
-$lang['imglib_invalid_path'] = 'The path to the image is not correct.';
-$lang['imglib_copy_failed'] = 'The image copy routine failed.';
-$lang['imglib_missing_font'] = 'Unable to find a font to use.';
-$lang['imglib_save_failed'] = 'Unable to save the image. Please make sure the image and file directory are writable.';
+$lang['imglib_source_image_required'] = 'Kailangan mong tukuyin ang pinagmulan ng imahe sa iyong preferences.';
+$lang['imglib_gd_required'] = 'Ang GD image library ay kinakailangan para sa feature na ito.';
+$lang['imglib_gd_required_for_props'] = 'Ang iyong server ay dapat sinusuportahan ang GD image library upang matukoy ang properties ng imahe.';
+$lang['imglib_unsupported_imagecreate'] = 'Ang iyong server ay hindi sinusuportahan ang GD function rna kinakailangan upang ma-proseso ang ganitong uri ng imahe.';
+$lang['imglib_gif_not_supported'] = 'Ang mga imaheng GIF ay madalas na hindi suportado dahil sa mga mahihigpit sa paglilisensya. Maaaring kailanganin mong gumamit ng imaheng JPG o PNG sa halip.';
+$lang['imglib_jpg_not_supported'] = 'Ang mga imaheng JPG ay hindi suportado.';
+$lang['imglib_png_not_supported'] = 'Ang mga imaheng PNG ay hindi suportado.';
+$lang['imglib_jpg_or_png_required'] = 'Ang resize protocol ng imahe na tinutukoy sa iyong preferences ay gumagana lamang sa mga imaheng JPEG o PNG.';
+$lang['imglib_copy_error'] = 'May error na naganap habang sinusubukang palitan ang file. Mangyaring siguraduhin na ang iyong directory ng file ay writable.';
+$lang['imglib_rotate_unsupported'] = 'Ang pag-ikot ng imahe ay tila hindi suportado ng iyong server';
+$lang['imglib_libpath_invalid'] = 'Ang path sa iyong image library ay hindi tama. Mangyaring i-set ang tamang path sa iyong image preferences.';
+$lang['imglib_image_process_failed'] = 'Nabigo ang pagproseso ng imahe. Mangyaring siguraduhin na sinusuportahan ng iyong server ang napiling protocol at tama ang path tungo sa iyong image library.';
+$lang['imglib_rotation_angle_required'] = 'Ang isang anggulo ng pag-ikot ang kinakailangan upang i-rotate ang larawan.';
+$lang['imglib_invalid_path'] = 'Ang path tungo sa imahe ay di wasto.';
+$lang['imglib_copy_failed'] = 'Nabigo ang routine ng pagkopya ng imahe.';
+$lang['imglib_missing_font'] = 'Hindi makahanap ng isang font na gagamitin.';
+$lang['imglib_save_failed'] = 'Hindi magawang i-save ang imahe. Mangyaring siguraduhin na ang mga directory ng imahe at file ay writable.';
