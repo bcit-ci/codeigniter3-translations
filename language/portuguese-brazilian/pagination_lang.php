f704c70066a338098f69a29f8319ad8a0cf18779
Marcelo Diniz <marcelo.leo27@gmail.com>
2015-03-10 10:23:19 -0300

    done more translations, corrections and punctuation

--- language/portuguese-brazilian/pagination_lang.php
+++ language/portuguese-brazilian/pagination_lang.php
@@ -12,4 +12,4 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $lang['pagination_first_link'] = '&lsaquo; Primeiro';
 $lang['pagination_next_link'] = '&gt;';
 $lang['pagination_prev_link'] = '&lt;';
-$lang['pagination_last_link'] = 'Útimo &rsaquo;';
+$lang['pagination_last_link'] = 'Último &rsaquo;';
