08e6bc8c12903853d68db40f674f437699c0d73d
Master Yoda <jim_parry@bcit.ca>
2015-03-11 09:53:21 -0700

    Fix remaining language bundles
    Used google translate to provide missing translations for form_validation
    Signed-off-by:Master Yoda <jim_parry@bcit.ca>

--- language/turkish/form_validation_lang.php
+++ language/turkish/form_validation_lang.php
@@ -37,4 +37,5 @@ $lang['form_validation_less_than_equal_to']     = '{field} alanı {param} den k�
 $lang['form_validation_greater_than']           = '{field} alanı {param} den büyük bir sayı içermeli.';
 $lang['form_validation_greater_than_equal_to']  = '{field} alanı {param} den büyük veya eşit bir sayı içermeli.';
 $lang['form_validation_error_message_not_set']  = '{field} alanı için bir hata mesajı bulunamadı.';
-$lang['form_validation_in_list']                = 'The {field} field must be one of: {param}.'; //FIXME
+//$lang['form_validation_in_list']                = 'The {field} field must be one of: {param}.'; //FIXME
+$lang['form_validation_in_list']                = '{param} : {field} field alan biri olmalıdır'; //google translated
