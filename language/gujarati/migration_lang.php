fe2d03a88f7609ebf6491735b18658a5656efd7a
sajid19991 <s_mathupotra@yahoo.co.in>
2015-03-15 04:50:23 +0530

    commit update migration_lang.php

--- language/gujarati/migration_lang.php
+++ language/gujarati/migration_lang.php
@@ -37,11 +37,11 @@
  */
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$lang['migration_none_found'] = 'No migrations were found.';
-$lang['migration_not_found'] = 'No migration could be found with the version number: %s.';
-$lang['migration_sequence_gap'] = 'There is a gap in the migration sequence near version number: %s.';
-$lang['migration_multiple_version'] = 'There are multiple migrations with the same version number: %s.';
-$lang['migration_class_doesnt_exist'] = 'The migration class "%s" could not be found.';
-$lang['migration_missing_up_method'] = 'The migration class "%s" is missing an "up" method.';
-$lang['migration_missing_down_method'] = 'The migration class "%s" is missing a "down" method.';
-$lang['migration_invalid_filename'] = 'Migration "%s" has an invalid filename.';
+$lang['migration_none_found'] = 'કોઈ સ્થળાંતર ના મળ્યા.';
+$lang['migration_not_found'] = 'કોઈ સ્થળાંતર આવૃત્તિ નંબર %s સાથે ના મળ્યા.';
+$lang['migration_sequence_gap'] = 'આવૃત્તિ નંબર %s ના નજીક સ્થળાંતર શ્રેણીમાં એક તફાવત છે.';
+$lang['migration_multiple_version'] = 'એક જ આવૃત્તિ નંબર %s સાથે ગુણજ સ્થળાંતર થયા.';
+$lang['migration_class_doesnt_exist'] = 'સ્થળાંતર વર્ગ "%s" ના મળ્યુ.';
+$lang['migration_missing_up_method'] = 'સ્થળાંતર વર્ગ "%s" માં એક "up" પદ્ધતિ ગુમ થયેલ છે.';
+$lang['migration_missing_down_method'] = 'સ્થળાંતર વર્ગ "%s" માં એક "down" પદ્ધતિ ગુમ થયેલ છે.';
+$lang['migration_invalid_filename'] = 'સ્થળાંતર "%s" અમાન્ય ફાઇલનામ છે.';
