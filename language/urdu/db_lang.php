8ea0f047a89f961d3ff2d50f7cf8d6dd25487cf2
codeonion <ali@codeonion.com>
2015-03-17 03:44:55 +0500

    Added Urdu translation of db_lang.php
    Signed-off-by: codeonion <ali@codeonion.com>

---
+++ language/urdu/db_lang.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * System messages translation for CodeIgniter(tm)
+ *
+ * @author CodeIgniter community
+ * @author Muhammad Ali
+ * @copyright Copyright (c) 2014 - 2015, British Columbia Institute of Technology (http://bcit.ca/)
+ * @license http://opensource.org/licenses/MIT MIT License
+ * @link http://codeigniter.com
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['db_invalid_connection_str']	= 'آپ نے جمع کروائی کنکشن سٹرنگ کی بنیاد پر ڈیٹا بیس کی ترتیبات کا تعین کرنے سے قاصر ہے.';
+$lang['db_unable_to_connect'] = 'فراہم کی ترتیبات استعمال کرتے ہوئے آپ کے ڈیٹا بیس سرور سے رابطہ کرنے سے قاصر ہے.';
+$lang['db_unable_to_select'] = 'مخصوص کردہ ڈیٹا بیس کو منتخب کرنے کے قابل نہیں: %s';
+$lang['db_unable_to_create'] = 'مخصوص کردہ ڈیٹابیس بنانے کے قابل نہیں: %s';
+$lang['db_invalid_query'] = 'آپ کی جمع کروائی کویری درست نہیں ہے.';
+$lang['db_must_set_table'] = 'کویری کے ساتھ استیمال کئے جانے والا ڈیٹابیس ٹیبل بتانا زروری حے۔';
+$lang['db_must_use_set'] = 'آپ کو ایک اندراج اپ ڈیٹ کرنے کےلئے "set" کے طریقہ کار کا استعمال کرنا چاہئے.';
+$lang['db_must_use_index'] = 'بیچ اپ ڈیٹس کے ملنے کے لئے ایک انڈیکس کی وضاحت لازمی ہے.';
+$lang['db_batch_missing_index'] = 'بیچ اپ ڈیٹ کرنے کے جمع کرائے گئے ایک یا ایک سے زیادہ قطار سے مخصوص انڈیکس غائب ہے.';
+$lang['db_must_use_where'] = 'جب تک کوئی "where" کا شق نہیں ہے، اپ ڈیٹس منع ہیں.';
+$lang['db_del_must_use_where'] = 'جب تک کوئی "where" یا "like" کا شق نہیں ہے، اکراج منع ھے.';
+$lang['db_field_param_missing'] = 'فیلڈ کو حاصل کرنے کے لئے ایک پیرامیٹر کے طور پر ٹیبل کے نام کی ضرورت ہوتی ہے.';
+$lang['db_unsupported_function'] = 'جو سھولت آب استعمیل کر رہے ہیں وہ اس ڈیٹابیس کے لیے نھیں ہے۔';
+$lang['db_transaction_failure'] = 'ٹرانزیکشن کی ناکامی: پرانا ورژن دوبارہ تعینات کر دیا گیا ہے.';
+$lang['db_unable_to_drop'] = 'مخصوص کردہ ڈیٹا بیس ڈراپ کرنے سے قاصر ہے.';
+$lang['db_unsupported_feature'] = 'آپ کے زیر استعمال ڈیٹا بیس پلیٹ فارم کی کی خصوصیت غیر معاون ہے.';
+$lang['db_unsupported_compression'] = 'آپ کا انتخاب کیا فائل کمپریشن فارمیٹ آپ کے سرور کی طرف سے معاون نہیں ہے.';
+$lang['db_filepath_error'] = 'ڈیٹا آپ کی فراہم کردہ فائل پاتھ پر لکھا نہیں جا سکتا.';
+$lang['db_invalid_cache_path'] = 'آپ نے جمع کروائی کیشے پاتھ درست یا قابل تحریر نہیں ہے.';
+$lang['db_table_name_required'] = 'ایک ٹیبل کا نام اس کارروائی کے لئے ضروری ہے.';
+$lang['db_column_name_required'] = 'ایک کالم کا نام اس کارروائی کے لئے ضروری ہے.';
+$lang['db_column_definition_required'] = 'ایک کالم کی تعریف اس کارروائی کے لئے ضروری ہے.';
+$lang['db_unable_to_set_charset'] = 'کلائنٹ کنکشن کیریکٹر سیٹ قائم کرنے کے قابل نہیں: %s';
+$lang['db_error_heading'] = 'ڈیٹا بیس میں ایک خرابی واقع ہوئی ہے';
