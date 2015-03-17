233f44b02834420aadf899aedb13d2409b1b9ea6
codeonion <ali@codeonion.com>
2015-03-17 07:03:15 +0500

    Added imglib_lang urdu Translations
    Signed-off-by: codeonion <ali@codeonion.com>

---
+++ language/urdu/imglib_lang.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * System messages translation for CodeIgniter(tm)
+ *
+ * @author CodeIgniter community
+ * @author Peter Denk
+ * @copyright Copyright (c) 2014 - 2015, British Columbia Institute of Technology (http://bcit.ca/)
+ * @license http://opensource.org/licenses/MIT MIT License
+ * @link http://codeigniter.com
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['imglib_source_image_required']	= 'اپ کے لیا لازم ہے کے پرفرنکس میں تصویر کا ٹھکانہ باتیں.';
+$lang['imglib_gd_required']		= 'اس سہولت تو استعمال کرنے کے لیے GD امیج لائبریری کا ہونا ضروری ہے.';
+$lang['imglib_gd_required_for_props']	= 'اس تصویر کی خصوصیت کا تعین کرنے کیلئے ضروری ہے کے آپکے سرور میں GD امیج لائبریری کی سپورٹ موجود ہو.';
+$lang['imglib_unsupported_imagecreate']	= 'اس قسم کی تصویر پر عمل کرنے کے لیا آپکے سرور میں GD فنکشن کی حمایت موجود نہیں.';
+$lang['imglib_gif_not_supported']	= 'GIF تصاویر اکثر لائسنسنگ پابندیوں کی وجہ سے سپپورٹڈ نہیں ہیں. آپکو JPG یا پھر PNG تصویر استعمال کرنا ہونگی.';
+$lang['imglib_jpg_not_supported']	= 'JPG تصویر کی حمایت حاصل نہیں.';
+$lang['imglib_png_not_supported']	= 'PNG تصویر کی حمایت حاصل نہیں.';
+$lang['imglib_jpg_or_png_required']	= 'آپ کی ترجیحات میں مخصوص تصویر کا سائز تبدیل کرنے کا پروٹوکول صرف JPEG اور PNG کی قسم کی تصویر کے ساتھ کام کرتا ہے.';
+$lang['imglib_copy_error']		= 'فائل کو تبدیل کرنے کی کوشش کے دوران ایک خامی پیش آگئی. براےمہربانی اس بات کو یقینی بنا لیں که فائل کی ڈائرکٹری پر لکھنا ممکن ہے.';
+$lang['imglib_rotate_unsupported']	= 'ظاہر نہیں ہوتا کے آپکے سرور پر تصویر کو گھومانے  کی سہولت موجود ہو.';
+$lang['imglib_libpath_invalid']		= 'آپکی تصویر کی لائبریری کا پاتھ  درست نہیں. آپ کی تصویر کی ترجیحات میں صحیح پاتھ  قائم کریں.';
+$lang['imglib_image_process_failed']	= 'تصویر پروسیسنگ میں ناکامی ہوگی. آپ اس بات کی تصدیق کریں کے آپکے سرور کو منتخب کردہ پروٹوکول کی حمایت حاصل ہے اور آپکی امیج لائبریری کا پاتھ درست ہے.';
+$lang['imglib_rotation_angle_required']	= 'تصویر کو گھومنے کیلئے زاویے کی ضرورت ہوتی ہے.';
+$lang['imglib_invalid_path']		= 'تصویر کا پاتھ درست نہیں.';
+$lang['imglib_copy_failed']		= 'تصویر کو کاپی کرنے کی روٹھنے ناکام رہی.';
+$lang['imglib_missing_font']		= 'استعمال کرنے کے لئے ایک فونٹ تلاش کرنے میں ناکامی ہی.';
+$lang['imglib_save_failed']		= 'تصویر کو محفوظ کرنے میں ناکامی. گزارش ہے کہ اس بات کی تسدیک کرلیں کے تصویر اور فائل کی ڈائرکٹری پر لکھنا ممکن ہے.';
