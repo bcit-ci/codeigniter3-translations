3fcaffca5168b166a342d12ca35108a8b8cf838f
codeonion <ali@codeonion.com>
2015-03-17 07:19:31 +0500

    Re-added migration_lang Urdu Translation
    Signed-off-by: codeonion <ali@codeonion.com>

---
+++ language/urdu/migration_lang.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * System messages translation for CodeIgniter(tm)
+ *
+ * @author CodeIgniter community
+ * @author Muhammad Ali
+ * @copyright Copyright (c) 2014 - 2015, British Columbia Institute of Technology (http://bcit.ca/)
+ * @license http://opensource.org/licenses/MIT MIT License
+ * @link http://codeigniter.com
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['migration_none_found']		= 'کوئی منتقلی پائی نہیں گئی.';
+$lang['migration_not_found']		= 'کوئی منتقلی پائی نہیں گئی جہاں ورژن  نمبر تھا: %s.';
+$lang['migration_sequence_gap']		= 'منتقلی میں ایک تسسلسل پایا گیا ہے جہاں ورژن نمبر ہے: %s.';
+$lang['migration_multiple_version']	= 'ایک سے زیادہ منتقلی موجود ہیں جھان ورژن نمبر ہے: %s.';
+$lang['migration_class_doesnt_exist']	= 'مائیگریشن کلاس  "%s" نہیں  ملی.';
+$lang['migration_missing_up_method']	= 'مائیگریشن کلاس "%s"  "up" method موجود نہیں.';
+$lang['migration_missing_down_method']	= 'مائیگریشن کلاس "%s" میں ایک "down" method موجود نہیں.';
+$lang['migration_invalid_filename']	= 'مائیگریشن "%s" کی فائل کا نام غلط ہے.';
