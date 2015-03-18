7f891eb034b29d76d636de515d8cc4cc0482ec2a
shindesushilkumar <shindesushilkumar@gmail.com>
2015-03-18 20:17:32 +0530

    created migration_lang.php

---
+++ language/hindi/migration_lang.php
@@ -0,0 +1,19 @@
+<?php
+/**
+* System messages translation for CodeIgniter(tm)
+*
+* @author	CodeIgniter community
+* @copyright	Copyright (c) 2014 - 2015, British Columbia Institute of Technology (http://bcit.ca/)
+* @license	http://opensource.org/licenses/MIT MIT License
+* @link	http://codeigniter.com
+*/
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['migration_none_found'] = 'No migrations were found.';
+$lang['migration_not_found'] = 'No migration could be found with the version number: %s.';
+$lang['migration_sequence_gap'] = 'There is a gap in the migration sequence near version number: %s.';
+$lang['migration_multiple_version'] = 'There are multiple migrations with the same version number: %s.';
+$lang['migration_class_doesnt_exist'] = 'The migration class "%s" could not be found.';
+$lang['migration_missing_up_method'] = 'The migration class "%s" is missing an "up" method.';
+$lang['migration_missing_down_method'] = 'The migration class "%s" is missing a "down" method.';
+$lang['migration_invalid_filename'] = 'Migration "%s" has an invalid filename.';
