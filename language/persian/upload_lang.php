819fa51a3685a35501dade0d55f43f6aec602b65
Sobhan Attar <sobhanattar@gmail.com>
2015-03-20 22:51:10 +0330

    Update upload_lang.php
    Do some corrections

--- language/persian/upload_lang.php
+++ language/persian/upload_lang.php
@@ -9,19 +9,19 @@
  */
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$lang['upload_userfile_not_set'] = 'عدم موفقیت در پیدا کردن یک متغیر که در post باشد و نام آن نیز userfile باشد. ';
+$lang['upload_userfile_not_set'] = 'متغیر post با نام userfile یافت نشد.';
 $lang['upload_file_exceeds_limit'] = 'اندازه فایل آپلود شده بیشتر از مقدار تعریف شده در تنظیمات PHP است.';
-$lang['upload_file_exceeds_form_limit'] = 'اندازه فایل آپلود شده بیشتر از مقدار تعریف شده در فرم است.';
-$lang['upload_file_partial'] = 'فقط قسمتی از فایل آپلود شده است . ';
-$lang['upload_no_temp_directory'] = 'temporary folder موجود نیست . ';
-$lang['upload_unable_to_write_file'] = 'فایل نمی تونه بر روی دیسک نوشته بشه!!! ';
-$lang['upload_stopped_by_extension'] = 'آپلود فایل توسط extension متوقف شده است.  ';
-$lang['upload_no_file_selected'] = 'یک فایل برای آپلود انتخاب کیند. ';
-$lang['upload_invalid_filetype'] = 'نوع فایلی که می خواهید آپلود کنید مجاز نمی باشد.  ';
+$lang['upload_file_exceeds_form_limit'] = 'اندازه فایل آپلود شده بیشتر از مقدار تعریف شده در فرم ارسال کننده آن است.';
+$lang['upload_file_partial'] = 'تنها قسمتی از فایل آپلود شده است.';
+$lang['upload_no_temp_directory'] = 'پوشه موقت(برای بارگذاری فایل) یافت نشد.';
+$lang['upload_unable_to_write_file'] = 'امکان نوشتن فایل روی حافظه وجود ندارد.';
+$lang['upload_stopped_by_extension'] = 'آپلود فایل به دلیل پسوند آن متوقف شده است.';
+$lang['upload_no_file_selected'] = 'فایلی را برای آپلود انتخاب کنید.';
+$lang['upload_invalid_filetype'] = 'نوع فایل انتخابی برای آپلود مجاز نیست.';
 $lang['upload_invalid_filesize'] = 'اندازه فایل آپلود شده بیشتر از مقدار مجاز است.';
-$lang['upload_invalid_dimensions'] = 'عکسی که می خواهید آپلود کنید با اندازه های مجاز هم خوانی ندارد.';
-$lang['upload_destination_error'] = 'موقعی که فایل را به مقصد نهایی انتقال می دادیم مشکلی پیش آمده است.  ';
-$lang['upload_no_filepath'] = 'آدرس مورد نظر برای اپلود فایل معتبر نمی باشد.';
+$lang['upload_invalid_dimensions'] = 'تصویری که می خواهید آپلود کنید با اندازه های مجاز هم خوانی ندارد.';
+$lang['upload_destination_error'] = 'در زمان انتقال فایل آپلود شده به مقصد نهایی مشکلی بروز کرده است.';
+$lang['upload_no_filepath'] = 'آدرس مورد نظر برای آپلود فایل معتبر نمی باشد.';
 $lang['upload_no_file_types'] = 'شما هیچ نوع فایل مجازی را انتخاب نکرده اید.';
-$lang['upload_bad_filename'] = 'یک فایل هم نام با فایل آپلود شده موجود می باشد.';
-$lang['upload_not_writable'] = 'پوشه مشخص شده برای آپلود قابل نوشتن نیست.  ';
+$lang['upload_bad_filename'] = 'یک فایل هم نام با فایل آپلود شده روی سرور موجود است.';
+$lang['upload_not_writable'] = 'پوشه مشخص شده برای آپلود قابل نوشتن نیست.';
