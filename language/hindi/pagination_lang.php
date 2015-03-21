5f8e00cac40990c0c3fbdb6f77bf046359565bea
shindesushilkumar <shindesushilkumar@gmail.com>
2015-03-21 11:52:59 +0530

    updated pagination_lang.php

--- language/hindi/pagination_lang.php
+++ language/hindi/pagination_lang.php
@@ -8,3 +8,8 @@
 * @link	http://codeigniter.com
 */
 defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['pagination_first_link'] = '&lsaquo; प्रथम';
+$lang['pagination_next_link'] = '&gt;';
+$lang['pagination_prev_link'] = '&lt;';
+$lang['pagination_last_link'] = 'आखरी &rsaquo;';
