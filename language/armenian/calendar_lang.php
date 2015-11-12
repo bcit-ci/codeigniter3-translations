19760cd5864bc91beda7d13f10787a2c72dee2ea
Tigran Muradyan <tgrnmrdn@mail.ru>
2015-11-13 02:15:03 +0400

    Armenian language adding (used english as base) and calendar_lang.php translation

---
+++ language/armenian/calendar_lang.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * System messages translation for CodeIgniter(tm)
+ *
+ * @author	CodeIgniter community
+ * @copyright	Copyright (c) 2014 - 2015, British Columbia Institute of Technology (http://bcit.ca/)
+ * @license	http://opensource.org/licenses/MIT	MIT License
+ * @link	http://codeigniter.com
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['cal_su'] = 'Կիր';
+$lang['cal_mo'] = 'Երկ';
+$lang['cal_tu'] = 'Երեք';
+$lang['cal_we'] = 'Չոր';
+$lang['cal_th'] = 'Հինգ';
+$lang['cal_fr'] = 'Ուրբ';
+$lang['cal_sa'] = 'Շաբ';
+$lang['cal_sun'] = 'Կիր';
+$lang['cal_mon'] = 'Երկ';
+$lang['cal_tue'] = 'Երեք';
+$lang['cal_wed'] = 'Չոր';
+$lang['cal_thu'] = 'Հինգ';
+$lang['cal_fri'] = 'Ուրբ';
+$lang['cal_sat'] = 'Շաբ';
+$lang['cal_sunday'] = 'Կիրակի';
+$lang['cal_monday'] = 'Երկուշաբթի';
+$lang['cal_tuesday'] = 'Երեքշաբթի';
+$lang['cal_wednesday'] = 'Չորեքշաբթի';
+$lang['cal_thursday'] = 'Հինգշաբթի';
+$lang['cal_friday'] = 'Ուրբաթ';
+$lang['cal_saturday'] = 'Շաբաթ';
+$lang['cal_jan'] = 'Հունվ';
+$lang['cal_feb'] = 'Փետր';
+$lang['cal_mar'] = 'Մարտ‎';
+$lang['cal_apr'] = 'Ապրի';
+$lang['cal_may'] = 'Մայի';
+$lang['cal_jun'] = 'Հունի';
+$lang['cal_jul'] = 'Հուլի';
+$lang['cal_aug'] = 'Օգոս';
+$lang['cal_sep'] = 'Սեպտ';
+$lang['cal_oct'] = 'Հոկտ';
+$lang['cal_nov'] = 'Նոյե';
+$lang['cal_dec'] = 'Դեկտ';
+$lang['cal_january'] = 'Հունվար‎';
+$lang['cal_february'] = 'Փետրվար‎';
+$lang['cal_march'] = 'Մարտ‎';
+$lang['cal_april'] = 'Ապրիլ‎';
+$lang['cal_mayl'] = 'Մայիս‎';
+$lang['cal_june'] = 'Հունիս‎';
+$lang['cal_july'] = 'Հուլիս‎';
+$lang['cal_august'] = 'Օգոստոս‎';
+$lang['cal_september'] = 'Սեպտեմբեր';
+$lang['cal_october'] = 'Հոկտեմբեր‎';
+$lang['cal_november'] = 'Նոյեմբեր‎';
+$lang['cal_december'] = 'Դեկտեմբեր‎';
---
+++ language/armenian/ftp_lang.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * System messages translation for CodeIgniter(tm)
+ *
+ * @author	CodeIgniter community
+ * @copyright	Copyright (c) 2014 - 2015, British Columbia Institute of Technology (http://bcit.ca/)
+ * @license	http://opensource.org/licenses/MIT	MIT License
+ * @link	http://codeigniter.com
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['ftp_no_connection']		= 'Unable to locate a valid connection ID. Please make sure you are connected before performing any file routines.';
+$lang['ftp_unable_to_connect']		= 'Unable to connect to your FTP server using the supplied hostname.';
+$lang['ftp_unable_to_login']		= 'Unable to login to your FTP server. Please check your username and password.';
+$lang['ftp_unable_to_mkdir']		= 'Unable to create the directory you have specified.';
+$lang['ftp_unable_to_changedir']	= 'Unable to change directories.';
+$lang['ftp_unable_to_chmod']		= 'Unable to set file permissions. Please check your path.';
+$lang['ftp_unable_to_upload']		= 'Unable to upload the specified file. Please check your path.';
+$lang['ftp_unable_to_download']		= 'Unable to download the specified file. Please check your path.';
+$lang['ftp_no_source_file']		= 'Unable to locate the source file. Please check your path.';
+$lang['ftp_unable_to_rename']		= 'Unable to rename the file.';
+$lang['ftp_unable_to_delete']		= 'Unable to delete the file.';
+$lang['ftp_unable_to_move']		= 'Unable to move the file. Please make sure the destination directory exists.';
