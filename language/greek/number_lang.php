b272645c28516d4fff8b078f1fd22c0ba2c7291a
grigoris86 <xargrigoris@gmail.com>
2015-11-24 11:49:38 +0200

    add number file

---
+++ language/greek/number_lang.php
@@ -0,0 +1,7 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['terabyte_abbr'] = 'TB';
+$lang['gigabyte_abbr'] = 'GB';
+$lang['megabyte_abbr'] = 'MB';
+$lang['kilobyte_abbr'] = 'KB';
+$lang['bytes'] = 'Bytes';
