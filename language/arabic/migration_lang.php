c0768872f27f367533555be0d0823719ae03b157
Atef Ben Ali <atefBB@users.noreply.github.com>
2017-04-04 21:28:46 +0100

    fix typo

--- language/arabic/migration_lang.php
+++ language/arabic/migration_lang.php
@@ -14,6 +14,6 @@ $lang['migration_not_found'] = "تعذر العثور على عملية التر
 $lang['migration_sequence_gap'] = 'هناك فجوة بين تسلسل عمليات الترحيل عند الرقم : %s.';
 $lang['migration_multiple_version'] = "هناك عدة عمليات ترحيل لها نفس التعداد: %d.";
 $lang['migration_class_doesnt_exist'] = "تعذر العثور على صنف الترحيل \"%s\".";
-$lang['migration_missing_up_method'] = "صنيف الترحيل \"%s\" يفتقد إلى دالة 'up'.";
+$lang['migration_missing_up_method'] = "صنف الترحيل \"%s\" يفتقد إلى دالة 'up'.";
 $lang['migration_missing_down_method'] = "صنف الترحيل \"%s\" يفتقد إلى دالة 'down'.";
 $lang['migration_invalid_filename'] = "اسم الملف الخاص بالترحيل \"%s\" غير صحيح.";
