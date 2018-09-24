97d4c9323d01537f30e792851070a16b18973875
Anderson Unsonst <anderson.unsonst@capgemini.com>
2018-09-24 14:16:47 -0300

    add new line valid_base64 for brazilian portuguese

--- language/portuguese-brazilian/form_validation_lang.php
+++ language/portuguese-brazilian/form_validation_lang.php
@@ -15,6 +15,7 @@ $lang['form_validation_valid_email'] = 'O campo {field} deve conter um email vá
 $lang['form_validation_valid_emails'] = 'O campo {field} deve conter apenas emails válidos.';
 $lang['form_validation_valid_url'] = 'O campo {field} deve conter uma URL válida.';
 $lang['form_validation_valid_ip'] = 'O campo {field} deve conter um IP válido.';
+$lang['form_validation_valid_base64'] = 'O campo {field} deve conter um valor Base64 válido.';
 $lang['form_validation_min_length'] = 'O campo {field} deve ter pelo menos {param} caractere(s).';
 $lang['form_validation_max_length'] = 'O campo {field} ultrapassou o limite de {param} caractere(s).';
 $lang['form_validation_exact_length'] = 'O campo {field} deve conter exatamente {param} caractere(s).';
