742c89b6502c1c9ad946c724dd917c5a861436b4
Ander Goñi <agoni@danobatgroup.com>
2019-04-29 09:57:31 +0200

    basque language pack added

---
+++ language/basque/number_lang.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * System messages translation for CodeIgniter(tm)
+ *
+ * @author	CodeIgniter community
+ * @author	Ander Goñi
+ * @copyright	Copyright (c) 2014-2019, British Columbia Institute of Technology (https://bcit.ca/)
+ * @license	http://opensource.org/licenses/MIT	MIT License
+ * @link	https://codeigniter.com
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['terabyte_abbr'] = 'TB';
+$lang['gigabyte_abbr'] = 'GB';
+$lang['megabyte_abbr'] = 'MB';
+$lang['kilobyte_abbr'] = 'KB';
+$lang['bytes'] = 'Byte';
---
+++ language/basque/upload_lang.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * System messages translation for CodeIgniter(tm)
+ *
+ * @author	CodeIgniter community
+ * @author	Ander Goñi
+ * @copyright	Copyright (c) 2014-2019, British Columbia Institute of Technology (https://bcit.ca/)
+ * @license	http://opensource.org/licenses/MIT	MIT License
+ * @link	https://codeigniter.com
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['upload_userfile_not_set'] = 'Ezin da userfile izena duen post aldagai bat aurkitu.';
+$lang['upload_file_exceeds_limit'] = 'Igotako fitxategiak PHP ezarpenetan ezarritako gehienezko tamaina gainditzen du.';
+$lang['upload_file_exceeds_form_limit'] = 'Igotako fitxategiak datuorriak ezarritako gehienezko tamaina gainditzen du.';
+$lang['upload_file_partial'] = 'Fitxategia partzialki bakarrik igo da.';
+$lang['upload_no_temp_directory'] = 'Aldi baterako direktoriorik ez da.';
+$lang['upload_unable_to_write_file'] = 'Fitxategia ezin da diskoan gorde.';
+$lang['upload_stopped_by_extension'] = 'Luzapen batek gelditu dut fitxategia igotzea.';
+$lang['upload_no_file_selected'] = 'Ez duzu igotzeko fitxategirik autatu.';
+$lang['upload_invalid_filetype'] = 'Igo nahi duzun fitxategi mota ez dago baimendua.';
+$lang['upload_invalid_filesize'] = 'Igo nahi duzun fitxategiaren tamaina, baimenduatakoa baino handiagoa da.';
+$lang['upload_invalid_dimensions'] = 'Igo nahi duzun fitxategiak, baimendutako gehienezko altuera edo zabalera gainditzen du.';
+$lang['upload_destination_error'] = 'Arazoa gertatu da igotako fitxategia behin betiko kokapenera mugitzerakoan.';
+$lang['upload_no_filepath'] = 'Igotako fitxategien bidea ez da baliozkoa.';
+$lang['upload_no_file_types'] = 'Ez duzu zehaztu baimendutako inongo fitxategi motarik.';
+$lang['upload_bad_filename'] = 'Bidali duzun fitxategi izena dagoeneko ba da zerbitzarian.';
+$lang['upload_not_writable'] = 'Igotako fitxategien direktorioak ez du idazteko baimenik.';
