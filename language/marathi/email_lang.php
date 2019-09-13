37348970c7e954bc8bb163cc0c8cde6135b12356
Akshay Devrukhkar <55195572+devakshay-official@users.noreply.github.com>
2019-09-13 13:21:15 +0530

    Update email_lang.php

--- language/marathi/email_lang.php
+++ language/marathi/email_lang.php
@@ -8,23 +8,23 @@
 * @license	http://opensource.org/licenses/MIT MIT License
 * @link	https://codeigniter.com
 */
-defined('BASEPATH') OR exit('Directe toegang tot scripts is niet toegestaan');
-$lang['email_must_be_array'] = 'ईमेल सत्यापन विधि को  सरणी (array) भेजी जानी चाहिए।';
-$lang['email_invalid_address'] = 'अवैध ईमेल पता है:% s';
-$lang['email_attachment_missing'] = '% S: निम्नलिखित ईमेल संलग्नक (अटैचमेंट) पता लगाने में असमर्थ';
-$lang['email_attachment_unreadable'] = 'अटैचमेंट खोलने के  असमर्थ :% s';
-$lang['email_no_from'] = 'From हेडर के बिना मेल नहीं भेज सकते हैं।';
-$lang['email_no_recipients'] = 'To, CC, BCC  प्राप्तकर्ताओं को शामिल करना जरुरी है ';
-$lang['email_send_failure_phpmail'] = 'PHP mail() का उपयोग करके ईमेल  भेजने में असमर्थ। आपका सर्वर इस पद्धति का उपयोग करके मेल भेजने के लिए शायद  कॉन्फ़िगर नहीं किया गया है';
-$lang['email_send_failure_sendmail'] = 'PHP sendmail का उपयोग करके ईमेल  भेजने में असमर्थ। आपका सर्वर इस पद्धति का उपयोग करके मेल भेजने के लिए शायद  कॉन्फ़िगर नहीं किया गया है';
-$lang['email_send_failure_smtp'] = 'PHP SMTP  का उपयोग करके ईमेल  भेजने में असमर्थ। आपका सर्वर इस पद्धति का उपयोग करके मेल भेजने के लिए शायद  कॉन्फ़िगर नहीं किया गया है';
-$lang['email_sent'] = 'आपका संदेश सफलतापूर्वक दिए गए प्रोटोकॉल का उपयोग करके भेज दिया गया है:% s';
-$lang['email_no_socket'] = 'सेंडमेल करने के लिए सॉकेट खोलने में असमर्थ। सेटिंग्स की जाँच करें।';
-$lang['email_no_hostname'] = 'आपने SMTP होस्टनेम  निर्दिष्ट नहीं किया है';
-$lang['email_smtp_error'] = 'निम्नलिखित एसएमटीपी त्रुटि आई है:% s';
-$lang['email_no_smtp_unpw'] = 'त्रुटि: एसएमटीपी यूज़रनेम और पासवर्ड प्रदान करना जरुरी है';
-$lang['email_failed_smtp_login'] = 'Auth लॉग इन कमांड भेजने में विफल। त्रुटि:% s';
-$lang['email_smtp_auth_un'] = 'उपयोगकर्ता नाम को प्रमाणित करने में विफल। त्रुटि:% s';
-$lang['email_smtp_auth_pw'] = 'पासवर्ड प्रमाणित करने में विफल। त्रुटि:% s';
-$lang['email_smtp_data_failure'] = 'डेटा भेजने के लिए असमर्थ : % s';
-$lang['email_exit_status'] = 'एग्जिट स्थिति कोड है:% s';
+defined('BASEPATH') OR exit('स्क्रिप्टमध्ये थेट प्रवेश करण्यास परवानगी नाही');
+$lang['email_must_be_array'] = 'ईमेल प्रमाणीकरण पद्धत ऐरेमध्ये पाठविली जाणे आवश्यक आहे.';
+$lang['email_invalid_address'] = 'अवैध ईमेल पत्ता आहे:% s';
+$lang['email_attachment_missing'] = '% S: खालील ईमेल संलग्नक (अटॅचमेंट) शोधण्यात असमर्थ';
+$lang['email_attachment_unreadable'] = 'संलग्नक उघडण्यास असमर्थ :% s';
+$lang['email_no_from'] = 'From हेडरशिवाय मेल पाठवू शकत नाही.';
+$lang['email_no_recipients'] = 'To, CC, BCC प्राप्तकर्त्यांचा समावेश करणे आवश्यक आहे ';
+$lang['email_send_failure_phpmail'] = 'PHP mail() वापरून ईमेल पाठविण्यात असमर्थ. आपल्या सर्व्हरला ही पद्धत वापरून मेल पाठविण्यासाठी कॉन्फिगर केले जाऊ शकत नाही';
+$lang['email_send_failure_sendmail'] = 'PHP sendmail वापरून ईमेल पाठविण्यात असमर्थ. आपल्या सर्व्हरला ही पद्धत वापरून मेल पाठविण्यासाठी कॉन्फिगर केले जाऊ शकत नाही';
+$lang['email_send_failure_smtp'] = 'PHP SMTP वापरून ईमेल पाठविण्यात असमर्थ. आपल्या सर्व्हरला ही पद्धत वापरून मेल पाठविण्यासाठी कॉन्फिगर केले जाऊ शकत नाही';
+$lang['email_sent'] = 'आपला संदेश दिलेल्या प्रोटोकॉलचा वापर करुन यशस्वीरित्या पाठविला गेला आहे:% s';
+$lang['email_no_socket'] = 'मेल पाठविण्यासाठी सॉकेट उघडण्यात अक्षम. सेटिंग्ज तपासा.';
+$lang['email_no_hostname'] = 'आपण एसएमटीपी होस्टनाव निर्दिष्ट केलेले नाही';
+$lang['email_smtp_error'] = 'खालील एसएमटीपी त्रुटी आली आहे:% s';
+$lang['email_no_smtp_unpw'] = 'त्रुटी: एसएमटीपी वापरकर्तानाव आणि संकेतशब्द प्रदान करणे आवश्यक आहे';
+$lang['email_failed_smtp_login'] = 'Auth लॉगिन आदेश पाठविण्यात अयशस्वी. त्रुटी:% s';
+$lang['email_smtp_auth_un'] = 'वापरकर्तानाव प्रमाणीकरण करण्यात अयशस्वी. त्रुटी:% s';
+ $lang['email_smtp_auth_pw'] = 'संकेतशब्द प्रमाणित करण्यात अयशस्वी. त्रुटी:% s';
+$lang['email_smtp_data_failure'] = 'डेटा पाठविण्यासाठी असमर्थ.: % s';
+$lang['email_exit_status'] = 'निर्गमन स्थिती कोड आहे:% s';
