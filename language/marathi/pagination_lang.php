6d73b55e6d7afae4c12bd07133752423b4634189
Akshay Devrukhkar <55195572+devakshay-official@users.noreply.github.com>
2019-09-16 12:31:12 +0530

    Update pagination_lang.php

--- language/marathi/pagination_lang.php
+++ language/marathi/pagination_lang.php
@@ -7,9 +7,9 @@
 * @license	http://opensource.org/licenses/MIT MIT License
 * @link	https://codeigniter.com
 */
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') OR exit('थेट स्क्रिप्ट प्रवेश परवानगी नाही');
 
-$lang['pagination_first_link'] = '&lsaquo; प्रथम';
+$lang['pagination_first_link'] = '&lsaquo; पहिला';
 $lang['pagination_next_link'] = '&gt;';
 $lang['pagination_prev_link'] = '&lt;';
-$lang['pagination_last_link'] = 'आखरी &rsaquo;';
+$lang['pagination_last_link'] = 'शेवटचा &rsaquo;';
