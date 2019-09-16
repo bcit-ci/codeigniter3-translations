7a21549727c8407b3f92f8a2a6f306057124a4aa
Akshay Devrukhkar <55195572+devakshay-official@users.noreply.github.com>
2019-09-16 12:42:42 +0530

    Update profiler_lang.php

--- language/marathi/profiler_lang.php
+++ language/marathi/profiler_lang.php
@@ -7,26 +7,26 @@
 * @license	http://opensource.org/licenses/MIT MIT License
 * @link	https://codeigniter.com
 */
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') OR exit('थेट स्क्रिप्ट प्रवेश परवानगी नाही');
 
 $lang['profiler_database'] = 'डेटाबेस';
-$lang['profiler_controller_info'] = 'वर्ग / विधि';
+$lang['profiler_controller_info'] = 'वर्ग / पद्धत';
 $lang['profiler_benchmarks'] = 'मानक';
-$lang['profiler_queries'] = 'सवाल';
+$lang['profiler_queries'] = 'प्रश्न';
 $lang['profiler_get_data'] = 'GET डेटा';
 $lang['profiler_post_data'] = 'POST डेटा';
-$lang['profiler_uri_string'] = 'URI शृंखला';
-$lang['profiler_memory_usage'] = 'मेमरी उपयोग';
-$lang['profiler_config'] = 'कॉन्फ़िग वेरिएबल्स';
+$lang['profiler_uri_string'] = 'URI मालिका';
+$lang['profiler_memory_usage'] = 'मेमरी वापर';
+$lang['profiler_config'] = 'कॉन्फ़िग व्हेरिएबल्स';
 $lang['profiler_session_data'] = 'SESSION डेटा';
 $lang['profiler_headers'] = 'HTTP हेडर्स';
-$lang['profiler_no_db'] = 'डाटाबेस ड्राइवर इस वक़्त लोड नहीं है';
-$lang['profiler_no_queries'] = 'कोई प्रश्न नहीं चल रहे थे।';
-$lang['profiler_no_post'] = 'POST डेटा मौजूद नहीं है।';
-$lang['profiler_no_get'] = 'GET डेटा मौजूद नहीं है।';
-$lang['profiler_no_uri'] = 'URI मौजूद नहीं है।';
-$lang['profiler_no_memory'] = 'मेमोरी उपयोग अनुपलब्ध';
-$lang['profiler_no_profiles'] = 'प्रोफ़ाइल डेटा नहीं है - सभी प्रोफाइलर वर्गों को निष्क्रिय कर दिया गया है।';
-$lang['profiler_section_hide'] = 'छुपाके रखो';
-$lang['profiler_section_show'] = 'दिखाओ';
+$lang['profiler_no_db'] = 'डेटाबेस ड्राइव्हर सध्या लोड केलेला नाही';
+$lang['profiler_no_queries'] = 'कोणतेही प्रश्न चालू नव्हते';
+$lang['profiler_no_post'] = 'POST डेटा अस्तित्त्वात नाही.';
+$lang['profiler_no_get'] = 'GET डेटा अस्तित्त्वात नाही.';
+$lang['profiler_no_uri'] = 'URI अस्तित्त्वात नाही.';
+$lang['profiler_no_memory'] = 'मेमोरी वापर अनुपलब्ध';
+$lang['profiler_no_profiles'] = 'प्रोफाइल डेटा नाही - सर्व प्रोफाइलर वर्ग निष्क्रिय केले गेले आहेत.';
+$lang['profiler_section_hide'] = 'गुप्त ठेवा';
+$lang['profiler_section_show'] = 'दाखवा';
 $lang['profiler_seconds'] = 'सेकंड';
